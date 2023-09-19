9dd2f10d0fc9ac3fe69e643c74c43ef19be6bfe3
maina-david <waihura.m@gmail.com>
2023-09-20 01:25:48 +0300

    initial commit

--- components/user-auth-form.tsx
+++ components/user-auth-form.tsx
@@ -15,7 +15,7 @@ import { Label } from "@/components/ui/label"
 import { toast } from "@/components/ui/use-toast"
 import { Icons } from "@/components/icons"
 
-interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> { }
 
 type FormData = z.infer<typeof userAuthSchema>
 
@@ -42,7 +42,7 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
 
     setIsLoading(false)
 
-    if (!signInResult?.ok) {
+    if (signInResult?.error) {
       return toast({
         title: "Something went wrong.",
         description: "Your sign in request failed. Please try again.",
