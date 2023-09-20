e3e41cbd4d162d579854b09f18605e182fc3a83e
maina-david <waihura.m@gmail.com>
2023-09-21 00:17:29 +0300

    Update dependencies and add new features.
    Update the following dependencies:
    
    - `@protobufjs/path` from `1.1.1` to `1.1.2`
    - `@protobufjs/pool` from `1.0.2` to `1.1.0`
    - `@protobufjs/utf8` from `1.0.3` to `1.1.0`
    - `long` from `4.0.0` to `5.2.3`
    - `@types/node` from `18.17.17` to `20.4.7`
    
    Add the following new features:
    
    - Add support for sending emails using Nodemailer.

--- env.mjs
+++ env.mjs
@@ -16,6 +16,11 @@ export const env = createEnv({
     GOOGLE_CLIENT_SECRET: z.string().min(1),
     FACEBOOK_CLIENT_ID: z.string().min(1),
     FACEBOOK_CLIENT_SECRET: z.string().min(1),
+    EMAIL_SERVER_USER: z.string().min(1),
+    EMAIL_SERVER_PASSWORD: z.string().min(1),
+    EMAIL_SERVER_HOST: z.string().min(1),
+    EMAIL_SERVER_PORT: z.number().min(1),
+    EMAIL_FROM: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_APP_URL: z.string().min(1),
@@ -33,5 +38,10 @@ export const env = createEnv({
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     FACEBOOK_CLIENT_ID: process.env.FACEBOOK_CLIENT_ID,
     FACEBOOK_CLIENT_SECRET: process.env.FACEBOOK_CLIENT_SECRET,
+    EMAIL_SERVER_USER: process.env.EMAIL_SERVER_USER,
+    EMAIL_SERVER_PASSWORD: process.env.EMAIL_SERVER_PASSWORD,
+    EMAIL_SERVER_HOST: process.env.EMAIL_SERVER_HOST,
+    EMAIL_SERVER_PORT: process.env.EMAIL_SERVER_PORT,
+    EMAIL_FROM: process.env.EMAIL_FROM,
   },
 })
