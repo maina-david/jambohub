8fc62e432148ee411aa2cfc5d608424d9939b8b1
maina-david <waihura.m@gmail.com>
2023-09-20 21:56:10 +0300

    Update user account navigation links to point to new locations

--- components/user-account-nav.tsx
+++ components/user-account-nav.tsx
@@ -39,13 +39,10 @@ export function UserAccountNav({ user }: UserAccountNavProps) {
         </div>
         <DropdownMenuSeparator />
         <DropdownMenuItem asChild>
-          <Link href="/dashboard">Dashboard</Link>
+          <Link href="/billing">Billing</Link>
         </DropdownMenuItem>
         <DropdownMenuItem asChild>
-          <Link href="/dashboard/billing">Billing</Link>
-        </DropdownMenuItem>
-        <DropdownMenuItem asChild>
-          <Link href="/dashboard/settings">Settings</Link>
+          <Link href="/settings">Settings</Link>
         </DropdownMenuItem>
         <DropdownMenuSeparator />
         <DropdownMenuItem
