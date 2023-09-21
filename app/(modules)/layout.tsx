14179a2ebd416da24466dc63abc9d3c833a9df4b
maina-david <waihura.m@gmail.com>
2023-09-21 11:00:38 +0300

    company settup modal

--- app/(modules)/layout.tsx
+++ app/(modules)/layout.tsx
@@ -1,13 +1,37 @@
 import { notFound } from "next/navigation"
 
+import * as React from "react"
+
 import { dashboardConfig } from "@/config/hub"
-import { getCurrentUser } from "@/lib/session"
+import { getCurrentUser, getCurrentUserCompanies } from "@/lib/session"
 import { MainNav } from "@/components/main-nav"
 import { DashboardNav } from "@/components/nav"
 import { SiteFooter } from "@/components/site-footer"
 import { UserAccountNav } from "@/components/user-account-nav"
 import { ModeToggle } from "@/components/mode-toggle"
 import CompanySwitcher from "@/components/team-switcher"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
+import { Button } from "@/components/ui/button"
 
 interface DashboardLayoutProps {
   children?: React.ReactNode
@@ -17,9 +41,63 @@ export default async function DashboardLayout({
   children,
 }: DashboardLayoutProps) {
   const user = await getCurrentUser()
+  const companies = await getCurrentUserCompanies()
+  const [showNewCompanyDialog, setShowNewCompanyDialog] = React.useState(false)
 
   if (!user) {
     return notFound()
+  }
+
+  if (!companies.length) {
+    setShowNewCompanyDialog(true)
+    return (
+      <Dialog open={showNewCompanyDialog} onOpenChange={setShowNewCompanyDialog}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Create company</DialogTitle>
+            <DialogDescription>
+              Add a new company to manage products and customers.
+            </DialogDescription>
+          </DialogHeader>
+          <div>
+            <div className="space-y-4 py-2 pb-4">
+              <div className="space-y-2">
+                <Label htmlFor="name">Company name</Label>
+                <Input id="name" placeholder="Acme Inc." />
+              </div>
+              <div className="space-y-2">
+                <Label htmlFor="plan">Subscription plan</Label>
+                <Select>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select a plan" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="free">
+                      <span className="font-medium">Free</span> -{" "}
+                      <span className="text-muted-foreground">
+                        Trial for two weeks
+                      </span>
+                    </SelectItem>
+                    <SelectItem value="pro">
+                      <span className="font-medium">Pro</span> -{" "}
+                      <span className="text-muted-foreground">
+                        $9/month per user
+                      </span>
+                    </SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
+          </div>
+          <DialogFooter>
+            <Button variant="outline" onClick={() => setShowNewCompanyDialog(false)}>
+              Cancel
+            </Button>
+            <Button type="submit">Continue</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    )
   }
 
   return (
--- lib/session.ts
+++ lib/session.ts
@@ -1,9 +1,23 @@
 import { getServerSession } from "next-auth/next"
 
 import { authOptions } from "@/lib/auth"
+import { User } from "next-auth"
+import { db } from "@/lib/db"
 
 export async function getCurrentUser() {
   const session = await getServerSession(authOptions)
 
   return session?.user
+}
+
+export async function getCurrentUserCompanies() {
+  const user = (await getCurrentUser()) as User
+
+  const companies = await db.company.findMany({
+    where: {
+      ownerId: user.id,
+    },
+  })
+  
+  return companies
 }
