687c95c18c05014a7b0008900cbdc27eeb512a69
maina-david <waihura.m@gmail.com>
2023-09-21 22:31:32 +0300

    fix checkbox spacing

--- components/modals/company-modal.tsx
+++ components/modals/company-modal.tsx
@@ -94,7 +94,7 @@ export const CompanyModal = () => {
                 </FormItem>
               )}
             />
-            <div className="flex items-center space-x-2">
+            <div className="flex items-center space-x-2 space-y-2">
               <Checkbox id="defaultCompany" />
               <label
                 htmlFor="defaultCompany"
