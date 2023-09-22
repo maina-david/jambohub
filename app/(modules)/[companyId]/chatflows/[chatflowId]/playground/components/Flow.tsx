bdeacd71860f1f7a7e5ac18db857bdf2ae994f42
maina-david <waihura.m@gmail.com>
2023-09-23 01:20:40 +0300

    fix sample flow

--- app/(modules)/[companyId]/chatflows/[chatflowId]/playground/components/Flow.tsx
+++ app/(modules)/[companyId]/chatflows/[chatflowId]/playground/components/Flow.tsx
@@ -1,20 +1,67 @@
-"use client"
+import { useState, useCallback } from 'react';
+import ReactFlow, {
+  addEdge,
+  FitViewOptions,
+  applyNodeChanges,
+  applyEdgeChanges,
+  Node,
+  Edge,
+  OnNodesChange,
+  OnEdgesChange,
+  OnConnect,
+  DefaultEdgeOptions,
+  NodeTypes,
+} from 'reactflow';
 
-import React from 'react';
-import ReactFlow from 'reactflow';
+import CustomNode from './CustomNode';
 
-import 'reactflow/dist/style.css';
-
-const initialNodes = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+const initialNodes: Node[] = [
+  { id: '1', data: { label: 'Node 1' }, position: { x: 5, y: 5 } },
+  { id: '2', data: { label: 'Node 2' }, position: { x: 5, y: 100 } },
 ];
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+
+const initialEdges: Edge[] = [{ id: 'e1-2', source: '1', target: '2' }];
+
+const fitViewOptions: FitViewOptions = {
+  padding: 0.2,
+};
+
+const defaultEdgeOptions: DefaultEdgeOptions = {
+  animated: true,
+};
+
+const nodeTypes: NodeTypes = {
+  custom: CustomNode,
+};
 
 export default function Flow() {
+  const [nodes, setNodes] = useState<Node[]>(initialNodes);
+  const [edges, setEdges] = useState<Edge[]>(initialEdges);
+
+  const onNodesChange: OnNodesChange = useCallback(
+    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    [setNodes]
+  );
+  const onEdgesChange: OnEdgesChange = useCallback(
+    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+    [setEdges]
+  );
+  const onConnect: OnConnect = useCallback(
+    (connection) => setEdges((eds) => addEdge(connection, eds)),
+    [setEdges]
+  );
+
   return (
-    <div className="react-flow-container">
-      <ReactFlow nodes={initialNodes} edges={initialEdges} />
-    </div>
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+      fitView
+      fitViewOptions={fitViewOptions}
+      defaultEdgeOptions={defaultEdgeOptions}
+      nodeTypes={nodeTypes}
+    />
   );
 }
