a62215b2d60a80dd4158aad2b02b86ce9771ff4e
maina-david <waihura.m@gmail.com>
2023-09-23 01:42:54 +0300

    fix sample flow

--- app/(modules)/[companyId]/layout.tsx
+++ app/(modules)/[companyId]/layout.tsx
@@ -43,7 +43,7 @@ export default async function CompanyLayout({
               {siteConfig.name}
             </span>
           </Link>
-          <CompanySwitcher items={companies} className="hidden items-center space-x-2 md:flex" />
+          <CompanySwitcher items={companies} className="hidden space-x-2 md:flex" />
           <div className="ml-auto flex items-center space-x-4">
             <MainNav items={dashboardConfig.mainNav} />
             <ModeToggle />
