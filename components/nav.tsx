6b695cafc2774a4fc3820ff54ef8396a26991e50
maina-david <waihura.m@gmail.com>
2023-09-23 01:27:41 +0300

    update nav to show active module on nested pages

--- components/nav.tsx
+++ components/nav.tsx
@@ -69,7 +69,7 @@ export function SideNav({
               <span
                 className={cn(
                   "group flex items-center rounded-md px-3 py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground",
-                  path === item.href ? "bg-accent" : "transparent",
+                  path?.startsWith(item.href) ? "bg-accent" : "transparent",
                   item.disabled && "cursor-not-allowed opacity-80"
                 )}
               >
