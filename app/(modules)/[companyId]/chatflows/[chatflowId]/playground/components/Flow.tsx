2b7ff2a88fe5f684bd98c454397121b59d58cc7d
maina-david <waihura.m@gmail.com>
2023-09-23 00:28:48 +0300

    fix sample flow

--- app/(modules)/[companyId]/chatflows/[chatflowId]/playground/components/Flow.tsx
+++ app/(modules)/[companyId]/chatflows/[chatflowId]/playground/components/Flow.tsx
@@ -1,43 +1,18 @@
-'use client'
+import React from 'react';
+import ReactFlow from 'reactflow';
 
-import { useCallback } from 'react'
-import ReactFlow, {
-  MiniMap,
-  Controls,
-  Background,
-  useNodesState,
-  useEdgesState,
-  addEdge,
-} from 'reactflow'
-
-import 'reactflow/dist/style.css'
+import 'reactflow/dist/style.css';
 
 const initialNodes = [
   { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
   { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
-]
+];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
 
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }]
-
-function Flow() {
-  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes)
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges)
-
-  const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), [setEdges])
-
+export default function App() {
   return (
-    <ReactFlow
-      nodes={nodes}
-      edges={edges}
-      onNodesChange={onNodesChange}
-      onEdgesChange={onEdgesChange}
-      onConnect={onConnect}
-    >
-      <MiniMap />
-      <Controls />
-      <Background />
-    </ReactFlow>
-  )
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <ReactFlow nodes={initialNodes} edges={initialEdges} />
+    </div>
+  );
 }
-
-export default Flow
