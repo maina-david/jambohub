80a7bc946d3640272df86045df685f964116a998
maina-david <waihura.m@gmail.com>
2023-09-23 12:56:42 +0300

    Fix missing components

--- app/(modules)/[companyId]/chatflows/[chatflowId]/playground/page.tsx
+++ app/(modules)/[companyId]/chatflows/[chatflowId]/playground/page.tsx
@@ -6,8 +6,6 @@ import { Separator } from "@/components/ui/separator"
 
 import { PresetActions } from "./components/preset-actions"
 import { PresetSave } from "./components/preset-save"
-import { PresetSelector } from "./components/preset-selector"
-import { presets } from "./data/presets"
 
 import ChatFlow from "./components/ChatFlow"
 
@@ -22,7 +20,6 @@ export default function PlaygroundPage() {
         <div className="container flex flex-col items-start justify-between space-y-2 py-4 sm:flex-row sm:items-center sm:space-y-0 md:h-16">
           <h2 className="text-lg font-semibold">Playground</h2>
           <div className="ml-auto flex w-full space-x-2 sm:justify-end">
-            <PresetSelector presets={presets} />
             <PresetSave />
             <PresetActions />
           </div>
