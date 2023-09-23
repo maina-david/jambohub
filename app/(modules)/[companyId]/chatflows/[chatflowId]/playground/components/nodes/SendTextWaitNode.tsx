3f8c155500b42367261f964300ad500e25e0b88e
maina-david <waihura.m@gmail.com>
2023-09-23 15:02:28 +0300

    update select with options

--- app/(modules)/[companyId]/chatflows/[chatflowId]/playground/components/nodes/SendTextWaitNode.tsx
+++ app/(modules)/[companyId]/chatflows/[chatflowId]/playground/components/nodes/SendTextWaitNode.tsx
@@ -3,15 +3,11 @@ import { Label } from '@/components/ui/label'
 import { Textarea } from '@/components/ui/textarea'
 import {
   Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
 
-const SendTextNode = () => {
+const SendTextNodeWait = () => {
   const onDragStart = (event: React.DragEvent<HTMLDivElement>) => {
     event.dataTransfer.setData('application/reactflow', 'custom')
     event.dataTransfer.effectAllowed = 'move'
@@ -26,7 +22,7 @@ const SendTextNode = () => {
       <div
         className="grid w-full gap-1.5"
       >
-        <Label htmlFor="message-2">Send Text</Label>
+        <Label htmlFor="message-2">Send Text And Wait</Label>
         <Textarea disabled placeholder="Type your message here." id="message-2" />
         <Select>
           <SelectTrigger className="w-[180px]">
@@ -38,4 +34,4 @@ const SendTextNode = () => {
   )
 }
 
-export default SendTextNode
+export default SendTextNodeWait
