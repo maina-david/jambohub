6e82ea60988d3fb89bc140713915e37f86fed3bf
maina-david <waihura.m@gmail.com>
2023-09-23 12:12:09 +0300

    add draggable custom nodes

--- app/(modules)/[companyId]/chatflows/[chatflowId]/playground/components/SendText.tsx
+++ app/(modules)/[companyId]/chatflows/[chatflowId]/playground/components/SendText.tsx
@@ -1,40 +1,45 @@
-import React from 'react'
+import React from 'react';
 import {
   HoverCard,
   HoverCardContent,
   HoverCardTrigger,
-} from "@/components/ui/hover-card"
-import { Label } from "@/components/ui/label"
+} from '@/components/ui/hover-card';
+import { Label } from '@/components/ui/label';
+import { Textarea } from '@/components/ui/textarea';
 
 const SendText = () => {
-  const onDragStart = (event, nodeType) => {
-    event.dataTransfer.setData('application/reactflow', nodeType)
-    event.dataTransfer.effectAllowed = 'move'
-  }
+  const onDragStart = (event: React.DragEvent<HTMLDivElement>) => {
+    event.dataTransfer.setData('application/reactflow', 'custom');
+    event.dataTransfer.effectAllowed = 'move';
+  };
 
   return (
     <div className="grid gap-2 pt-2">
       <HoverCard openDelay={200}>
         <HoverCardTrigger asChild>
-          <div className="grid gap-4">
+          <div
+            className="grid gap-4"
+            draggable
+            onDragStart={(event) => onDragStart(event)}
+          >
             <div className="flex items-center justify-between">
               <Label htmlFor="maxlength">Send Text</Label>
             </div>
-            <div className="cursor:grab" onDragStart={(event) => onDragStart(event, 'custom')} draggable>
-              Send Text Node
+            <div>
+              <Textarea
+                placeholder="Type the text to be sent..."
+                rows={4}
+                className="w-full rounded border p-2"
+              ></Textarea>
             </div>
           </div>
         </HoverCardTrigger>
-        <HoverCardContent
-          align="start"
-          className="w-[260px] text-sm"
-          side="left"
-        >
+        <HoverCardContent align="start" className="w-[260px] text-sm" side="left">
           The reply message to be sent when a customer&apos;s message is received by our APIs
         </HoverCardContent>
       </HoverCard>
     </div>
-  )
-}
+  );
+};
 
-export default SendText
+export default SendText;
