c78b61acc7e131160101e6fdea2f2623e24b8c0d
maina-david <waihura.m@gmail.com>
2023-09-25 01:38:49 +0300

    fix channel layout

--- app/(modules)/[companyId]/channels/page.tsx
+++ app/(modules)/[companyId]/channels/page.tsx
@@ -18,13 +18,18 @@ const ChannelsPage = async ({ params }: { params: { companyId: string } }) => {
   return (
     <>
       {channels.length ? (
-        <>
-          <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="flex-1 space-y-4 p-8 pt-6">
+          <div className="flex items-center justify-between space-y-2">
+            <div className="flex items-center space-x-2">
+              <AddChannelModal />
+            </div>
+          </div>
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
             {channels.map((channel, index) => {
               return <ChannelCard key={index} data={channel} />
             })}
-          </ul>
-        </>
+          </div>
+        </div>
       ) : (
         <EmptyPlaceholder>
           <EmptyPlaceholder.Icon name="channels" />
