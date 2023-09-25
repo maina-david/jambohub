baef96a2ddf3c091b93405262c85238bccc32952
maina-david <waihura.m@gmail.com>
2023-09-25 11:30:35 +0300

    try new card

--- app/(modules)/[companyId]/channels/_components/channel-card.tsx
+++ app/(modules)/[companyId]/channels/_components/channel-card.tsx
@@ -16,7 +16,6 @@ import {
 } from "@/components/ui/card"
 import {
   DropdownMenu,
-  DropdownMenuCheckboxItem,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
@@ -27,6 +26,7 @@ import { Separator } from "@/components/ui/separator"
 import { format } from 'date-fns'
 import { Channel } from "@prisma/client"
 import { Trash2Icon } from "lucide-react"
+import Image from "next/image"
 
 interface ChannelProps {
   data: Channel
@@ -34,64 +34,44 @@ interface ChannelProps {
 
 export function ChannelCard({ data }: ChannelProps) {
   return (
-    <Card>
-      <CardHeader className="flex flex-col items-start gap-4 md:flex-row">
-        <div className="space-y-1">
-          <CardTitle>{data.name}</CardTitle>
-          <CardDescription>
-            {data.description}
-          </CardDescription>
+    <div className="w-full max-w-sm rounded-lg border border-gray-200 bg-white shadow dark:border-gray-700 dark:bg-gray-800">
+      <div className="flex justify-end px-4 pt-4">
+        <button id="dropdownButton" data-dropdown-toggle="dropdown" className="inline-block rounded-lg p-1.5 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-700" type="button">
+          <span className="sr-only">Open dropdown</span>
+          <svg className="h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 3">
+            <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z" />
+          </svg>
+        </button>
+        <div id="dropdown" className="z-10 hidden w-44 list-none divide-y divide-gray-100 rounded-lg bg-white text-base shadow dark:bg-gray-700">
+          <ul className="py-2" aria-labelledby="dropdownButton">
+            <li>
+              <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white">Edit</a>
+            </li>
+            <li>
+              <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white">Export Data</a>
+            </li>
+            <li>
+              <a href="#" className="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white">Delete</a>
+            </li>
+          </ul>
         </div>
-        <div className="flex items-center space-x-1 rounded-md bg-secondary text-secondary-foreground">
-          <Button variant="secondary" className="px-3 shadow-none">
-            <StarIcon className="mr-2 h-4 w-4" />
-            {data.type.toLowerCase()}
-          </Button>
-          <Separator orientation="vertical" className="h-[20px]" />
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="secondary" className="px-2 shadow-none">
-                <ChevronDownIcon className="h-4 w-4 text-secondary-foreground" />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent
-              align="end"
-              alignOffset={-5}
-              className="w-[200px]"
-              forceMount
-            >
-              <DropdownMenuLabel>Account Actions</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuCheckboxItem>
-                <Pencil2Icon className="mr-2 h-4 w-4" />
-                Link Account
-              </DropdownMenuCheckboxItem>
-              <DropdownMenuCheckboxItem>
-                <Pencil2Icon className="mr-2 h-4 w-4" />
-                Activate
-              </DropdownMenuCheckboxItem>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>
-                <Pencil2Icon className="mr-2 h-4 w-4" /> Edit Account
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <Trash2Icon className="mr-2 h-4 w-4" /> Delete Account
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+      </div>
+      <div className="flex flex-col items-center pb-10">
+        <Image
+          src={''}
+          alt={''}
+          width={24}
+          height={24}
+          className="mb-3 rounded-full shadow-lg"
+        />
+        <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">Bonnie Green</h5>
+        <span className="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
+        <div className="mt-4 flex space-x-3 md:mt-6">
+          <a href="#" className="inline-flex items-center rounded-lg bg-blue-700 px-4 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add friend</a>
+          <a href="#" className="inline-flex items-center rounded-lg border border-gray-300 bg-white px-4 py-2 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-700">Message</a>
         </div>
-      </CardHeader>
-      <CardContent>
-        <div className="flex space-x-4 text-sm text-muted-foreground">
-          <div className="flex items-center">
-            {data.identifier}
-          </div>
-          <div className="flex items-center">
-            {data.integrated ? 'Linked' : 'Not Linked'}
-          </div>
-          <div>Updated {format(data.updatedAt, 'MMMM d, yyyy h:mm a')}</div>
-        </div>
-      </CardContent>
-    </Card>
+      </div>
+    </div>
+
   )
 }
