426e2ec33e03927a4ae6ba5ce3d60f4da682c3b2
maina-david <waihura.m@gmail.com>
2023-09-25 11:54:16 +0300

    try new card

--- app/(modules)/[companyId]/channels/_components/channel-card.tsx
+++ app/(modules)/[companyId]/channels/_components/channel-card.tsx
@@ -11,11 +11,13 @@ import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
 import {
   DropdownMenu,
+  DropdownMenuCheckboxItem,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
@@ -25,7 +27,7 @@ import {
 import { Separator } from "@/components/ui/separator"
 import { format } from 'date-fns'
 import { Channel } from "@prisma/client"
-import { Trash2Icon } from "lucide-react"
+import { PencilIcon, Trash2Icon } from "lucide-react"
 import Image from "next/image"
 
 interface ChannelProps {
@@ -34,44 +36,59 @@ interface ChannelProps {
 
 export function ChannelCard({ data }: ChannelProps) {
   return (
-    <div className="w-full max-w-sm rounded-lg border border-gray-200 bg-white shadow dark:border-gray-700 dark:bg-gray-800">
-      <div className="flex justify-end px-4 pt-4">
-        <button id="dropdownButton" data-dropdown-toggle="dropdown" className="inline-block rounded-lg p-1.5 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-700" type="button">
-          <span className="sr-only">Open dropdown</span>
-          <svg className="h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 3">
-            <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z" />
-          </svg>
-        </button>
-        <div id="dropdown" className="z-10 hidden w-44 list-none divide-y divide-gray-100 rounded-lg bg-white text-base shadow dark:bg-gray-700">
-          <ul className="py-2" aria-labelledby="dropdownButton">
-            <li>
-              <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white">Edit</a>
-            </li>
-            <li>
-              <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white">Export Data</a>
-            </li>
-            <li>
-              <a href="#" className="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white">Delete</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className="flex flex-col items-center pb-10">
+    <Card>
+      <CardHeader className="flex justify-end px-4 pt-4">
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <svg
+              className="h-5 w-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 16 3"
+            >
+              <path
+                d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z"
+              />
+            </svg>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Account Actions</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Link Account</DropdownMenuItem>
+            <DropdownMenuItem>Activate</DropdownMenuItem>
+            <DropdownMenuItem>
+              <PencilIcon className="mr-2 h-2 w-2" />Edit
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Trash2Icon className="mr-2 h-2 w-2" />Delete
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </CardHeader>
+      <CardContent className="flex flex-col items-center pb-10">
         <Image
           src={'/images/icons/whatsapp.svg'}
           alt={'WhatsApp'}
-          width={24}
-          height={24}
+          width={34}
+          height={34}
           className="mb-3 rounded-full shadow-lg"
         />
-        <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">Bonnie Green</h5>
-        <span className="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
-        <div className="mt-4 flex space-x-3 md:mt-6">
-          <a href="#" className="inline-flex items-center rounded-lg bg-blue-700 px-4 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add friend</a>
-          <a href="#" className="inline-flex items-center rounded-lg border border-gray-300 bg-white px-4 py-2 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-700">Message</a>
-        </div>
-      </div>
-    </div>
-
+        <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{data.name}</h5>
+        <span className="text-sm text-gray-500 dark:text-gray-400">{data.description}</span>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button
+          variant={data.integrated ? 'destructive' : 'outline'}
+        >
+          {data.integrated ? 'Unlink' : 'Link'}
+        </Button>
+        <Button
+          variant={data.status ? 'destructive' : 'outline'}
+        >
+          {data.integrated ? 'Deactivate' : 'Activate'}
+        </Button>
+      </CardFooter>
+    </Card>
   )
 }
