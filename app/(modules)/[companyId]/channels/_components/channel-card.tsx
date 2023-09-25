5caff7c3d573f043a9a74fdc9dae046acc6b1aa1
maina-david <waihura.m@gmail.com>
2023-09-25 21:24:54 +0300

    check new card layout

--- app/(modules)/[companyId]/channels/_components/channel-card.tsx
+++ app/(modules)/[companyId]/channels/_components/channel-card.tsx
@@ -21,83 +21,61 @@ import { PencilIcon, Trash2Icon } from "lucide-react"
 import Image from "next/image"
 
 interface ChannelProps {
-  data: Channel
+  channel: Channel
 }
 
-export function ChannelCard({ data }: ChannelProps) {
+export function ChannelCard({ channel }: ChannelProps) {
   const channelModal = useChannelModal()
   const openEditModal = () => {
-    // Check if there is data to determine edit or create mode
-    if (data) {
-      // Edit mode: set the channel data
-      channelModal.setChannel(data)
+    // Check if there is channel to determine edit or create mode
+    if (channel) {
+      // Edit mode: set the channel
+      channelModal.setChannel(channel)
     } else {
-      // Create mode: clear any existing channel data
+      // Create mode: clear any existing channel
       channelModal.setChannel(null)
     }
 
     channelModal.onOpen()
   }
   return (
-    <Card className="w-full max-w-sm">
-      <CardHeader>
-        <div className="flex justify-end">
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button
-                variant="ghost"
-                className="inline-block"
-                size={'icon'}
-              >
-                <svg
-                  className="h-5 w-5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 16 3"
-                >
-                  <path
-                    d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z"
-                  />
-                </svg>
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuLabel>Account Actions</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem onClick={openEditModal}>
-                <PencilIcon className="mr-2 h-2 w-2" />Edit
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <Trash2Icon className="mr-2 h-2 w-2" />Delete
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+    <li className="col-span-1 divide-y divide-gray-200 rounded-lg shadow">
+      <div className="flex w-full items-center justify-between space-x-6 p-6">
+        <div className="flex-1 truncate">
+          <div className="flex items-center space-x-3">
+            <h3 className="truncate text-sm font-medium">{channel.name}</h3>
+            <span className="inline-flex shrink-0 items-center rounded-full bg-green-50 px-1.5 py-0.5 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+              {channel.type.toLowerCase()}
+            </span>
+          </div>
+          <p className="mt-1 truncate text-sm">{channel.description}</p>
         </div>
-      </CardHeader>
-      <CardContent className="flex flex-col items-center">
         <Image
-          src={`/images/icons/${data.type.toLowerCase()}.svg`}
-          alt={data.type.toLowerCase()}
-          width={34}
-          height={34}
-          className="mb-3 rounded-full shadow-lg"
+          className=" shrink-0 rounded-full bg-gray-300"
+          src={`/images/icons/${channel.type.toLowerCase()}.svg`}
+          alt={`${channel.type.toLowerCase()}`}
+          width={10}
+          height={10}
         />
-        <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{data.name}</h5>
-        <span className="text-sm text-gray-500 dark:text-gray-400">{data.description}</span>
-      </CardContent>
-      <CardFooter className="flex justify-between">
-        <Button
-          variant={data.integrated ? 'destructive' : 'outline'}
-        >
-          {data.integrated ? 'Unlink' : 'Link'}
-        </Button>
-        <Button
-          variant={data.status ? 'destructive' : 'outline'}
-        >
-          {data.status ? 'Deactivate' : 'Activate'}
-        </Button>
-      </CardFooter>
-    </Card>
+      </div>
+      <div>
+        <div className="-mt-px flex divide-x divide-gray-200">
+          <div className="flex w-0 flex-1">
+            <Button
+              variant={channel.integrated ? 'destructive' : 'outline'}
+            >
+              {channel.integrated ? 'Unlink' : 'Link'}
+            </Button>
+          </div>
+          <div className="-ml-px flex w-0 flex-1">
+            <Button
+              variant={channel.status ? 'destructive' : 'outline'}
+            >
+              {channel.status ? 'Deactivate' : 'Activate'}
+            </Button>
+          </div>
+        </div>
+      </div>
+    </li>
   )
 }
--- app/(modules)/[companyId]/channels/page.tsx
+++ app/(modules)/[companyId]/channels/page.tsx
@@ -26,11 +26,11 @@ const ChannelsPage = async ({ params }: { params: { companyId: string } }) => {
                 <ChannelModal />
               </div>
             </div>
-            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
+            <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
               {channels.map((channel, index) => {
                 return <ChannelCard key={index} data={channel} />
               })}
-            </div>
+            </ul>
           </div>
         ) : (
           <EmptyPlaceholder>
