20fd3c62e91fafd1fc8a7a9dc409796c9cb8821a
maina-david <waihura.m@gmail.com>
2023-09-26 12:12:40 +0300

    update header typography

--- components/header.tsx
+++ components/header.tsx
@@ -17,7 +17,7 @@ export function AppHeader({
     <>
       <div className="flex items-center justify-between px-2">
         <div className="grid gap-1">
-          <h2 className="text-3xl font-semibold tracking-tight transition-colors first:mt-0">{heading}</h2>
+          <h2 className="scroll-m-20 text-2xl font-semibold tracking-tight transition-colors first:mt-0">{heading}</h2>
           {text && <p className="text-muted-foreground">{text}</p>}
         </div>
         {children}
