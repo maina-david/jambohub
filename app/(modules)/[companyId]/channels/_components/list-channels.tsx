fa4751e4caa48d4730501f8f3984e07fce7022a9
maina-david <waihura.m@gmail.com>
2023-09-26 03:55:20 +0300

    update skeleton list

--- app/(modules)/[companyId]/channels/_components/list-channels.tsx
+++ app/(modules)/[companyId]/channels/_components/list-channels.tsx
@@ -20,11 +20,11 @@ export default function ListChannels() {
 
   if (isLoading) {
     return (
-      <div>
+      <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
         {Array.from({ length: 3 }).map((_, index) => (
           <ChannelSkeleton key={index} />
         ))}
-      </div>
+      </ul>
     )
   }
 
