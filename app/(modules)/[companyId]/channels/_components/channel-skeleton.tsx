1c9fafc5ad7d9e41cdb6356d8fa1d3b928507a54
maina-david <waihura.m@gmail.com>
2023-09-26 04:13:10 +0300

    update skeleton

--- app/(modules)/[companyId]/channels/_components/channel-skeleton.tsx
+++ app/(modules)/[companyId]/channels/_components/channel-skeleton.tsx
@@ -6,11 +6,11 @@ export default function ChannelSkeleton() {
   return (
     <li className="col-span-1 rounded-lg shadow-2xl">
       <div className="flex items-center space-x-4">
-        <Skeleton className="h-12 w-12 rounded-full" />
         <div className="space-y-2">
           <Skeleton className="h-4 w-[250px]" />
           <Skeleton className="h-4 w-[200px]" />
         </div>
+        <Skeleton className="h-12 w-12 rounded-full" />
       </div>
     </li>
   );
