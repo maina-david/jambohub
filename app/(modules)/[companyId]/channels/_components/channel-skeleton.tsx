3f7d87babb3be658fe813ec51507ebcfbce8d96d
maina-david <waihura.m@gmail.com>
2023-09-26 11:30:15 +0300

    fix skeleton

--- app/(modules)/[companyId]/channels/_components/channel-skeleton.tsx
+++ app/(modules)/[companyId]/channels/_components/channel-skeleton.tsx
@@ -5,12 +5,9 @@ import { Skeleton } from "@/components/ui/skeleton"
 export default function ChannelSkeleton() {
   return (
     <div className="flex items-center space-x-4">
-      <div className="flex items-center space-x-4">
-        <Skeleton className="h-12 w-12 rounded-full" />
-        <div className="space-y-2">
-          <Skeleton className="h-4 w-[250px]" />
-          <Skeleton className="h-4 w-[200px]" />
-        </div>
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[200px]" />
       </div>
       <Skeleton className="h-12 w-12 rounded-full" />
     </div>
