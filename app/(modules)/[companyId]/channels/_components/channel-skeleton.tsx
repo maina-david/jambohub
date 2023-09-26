a211c72e7ff57a9c0cb38b45b360303b4e9f5d36
maina-david <waihura.m@gmail.com>
2023-09-26 04:40:38 +0300

    update skeleton

--- app/(modules)/[companyId]/channels/_components/channel-skeleton.tsx
+++ app/(modules)/[companyId]/channels/_components/channel-skeleton.tsx
@@ -6,22 +6,19 @@ export default function ChannelSkeleton() {
   return (
     <li className="col-span-1 rounded-lg shadow-2xl">
       <div className="flex w-full items-center justify-between space-x-6 p-6">
-        <div className="flex-1 truncate">
-          <div className="flex items-center space-x-3">
-            <Skeleton className="h-4 w-[150px]" />
-          </div>
+        <div className="space-y-2">
           <Skeleton className="h-4 w-[250px]" />
-          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[200px]" />
         </div>
         <Skeleton className="h-12 w-12 rounded-full" />
       </div>
       <div>
         <div className="-mt-px flex">
           <div className="my-1 flex w-0 flex-1">
-            <Skeleton className="h-4 w-[50px]" />
+            <Skeleton className="relative -mr-px inline-flex h-4 w-[50px] flex-1 items-center justify-center gap-x-3 rounded-bl-lg py-4" />
           </div>
           <div className="my-1 flex w-0 flex-1">
-            <Skeleton className="h-4 w-[50px]" />
+            <Skeleton className="relative -mr-px inline-flex h-4 w-[50px] flex-1 items-center justify-center gap-x-3 rounded-bl-lg py-4" />
           </div>
         </div>
       </div>
