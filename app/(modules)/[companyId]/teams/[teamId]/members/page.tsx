31779270c649f5e114787815385e49d5c8a81fa2
maina-david <waihura.m@gmail.com>
2023-09-27 21:53:05 +0300

    add members table

--- app/(modules)/[companyId]/teams/[teamId]/members/page.tsx
+++ app/(modules)/[companyId]/teams/[teamId]/members/page.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import { Payment, columns } from "./columns"
+import { DataTable } from "./data-table"
 
-export default function TeamMembers() {
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    // ...
+  ]
+}
+
+export default async function TeamMembers() {
+  const data = await getData()
+
   return (
-    <div>TeamMemberspage</div>
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={data} />
+    </div>
   )
 }
