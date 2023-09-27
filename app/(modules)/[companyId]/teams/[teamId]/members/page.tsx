9ca8f21813ca553051814af49c130f5fd9c2ebb5
maina-david <waihura.m@gmail.com>
2023-09-27 22:14:04 +0300

    update members table

--- app/(modules)/[companyId]/teams/[teamId]/members/page.tsx
+++ app/(modules)/[companyId]/teams/[teamId]/members/page.tsx
@@ -1,22 +1,45 @@
+import * as React from "react"
+
 import { Payment, columns } from "./columns"
 import { DataTable } from "./data-table"
 
 async function getData(): Promise<Payment[]> {
-  // Fetch data from your API here.
   return [
     {
-      id: "728ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
+      id: "m5gr84i9",
+      amount: 316,
+      status: "success",
+      email: "ken99@yahoo.com",
     },
-    // ...
+    {
+      id: "3u1reuv4",
+      amount: 242,
+      status: "success",
+      email: "Abe45@gmail.com",
+    },
+    {
+      id: "derv1ws0",
+      amount: 837,
+      status: "processing",
+      email: "Monserrat44@gmail.com",
+    },
+    {
+      id: "5kma53ae",
+      amount: 874,
+      status: "success",
+      email: "Silas22@gmail.com",
+    },
+    {
+      id: "bhqecj4p",
+      amount: 721,
+      status: "failed",
+      email: "carmella@hotmail.com",
+    },
   ]
 }
 
-export default async function TeamMembers() {
+export default async function DataTableDemo() {
   const data = await getData()
-
   return (
     <div className="container mx-auto py-10">
       <DataTable columns={columns} data={data} />
