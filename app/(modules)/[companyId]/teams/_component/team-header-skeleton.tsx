9de32d07fb69c21e7b22e89cab6c27046544464b
maina-david <waihura.m@gmail.com>
2023-09-27 20:16:35 +0300

    updae header skeleton

--- app/(modules)/[companyId]/teams/_component/team-header-skeleton.tsx
+++ app/(modules)/[companyId]/teams/_component/team-header-skeleton.tsx
@@ -2,15 +2,19 @@
 
 import React from 'react'
 import { Skeleton } from '@/components/ui/skeleton'
+import { Separator } from '@/components/ui/separator'
 
 export default function TeamHeaderSkeleton() {
   return (
-    <div className="flex items-center justify-between px-2">
-      <div className="grid gap-1">
-        <Skeleton className="h-4 w-[250px]" />
-        <Skeleton className="h-4 w-[350px]" />
+    <>
+      <div className="flex items-center justify-between px-2">
+        <div className="grid gap-1">
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[350px]" />
+        </div>
+        <Skeleton className="h-4 w-[100px]" />
       </div>
-      <Skeleton className="h-4 w-[100px]" />
-    </div>
+      <Separator />
+    </>
   )
 }
