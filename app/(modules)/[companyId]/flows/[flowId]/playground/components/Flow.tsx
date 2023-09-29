f22c584bfb957aad3f62be2c6a75bb7b84a21544
maina-david <waihura.m@gmail.com>
2023-09-29 15:40:49 +0300

    update flow

--- app/(modules)/[companyId]/flows/[flowId]/playground/components/Flow.tsx
+++ app/(modules)/[companyId]/flows/[flowId]/playground/components/Flow.tsx
@@ -1,20 +1,20 @@
 'use client'
 
-import { useState, useCallback, useRef } from 'react'
+import React, { useCallback, useRef } from 'react'
 import ReactFlow, {
-  addEdge,
-  FitViewOptions,
-  applyNodeChanges,
-  applyEdgeChanges,
+  Controls,
+  OnConnectEnd,
+  OnConnectStart,
+  Panel,
+  useStoreApi,
   Node,
-  Edge,
-  OnNodesChange,
-  OnEdgesChange,
-  OnConnect,
-  DefaultEdgeOptions,
-  Controls, Panel, NodeOrigin, ReactFlowProvider
+  useReactFlow,
+  ReactFlowProvider,
+  NodeOrigin,
+  ConnectionLineType,
 } from 'reactflow'
-import { shallow } from 'zustand/shallow';
+
+import { shallow } from 'zustand/shallow'
 
 import useStore, { RFState } from '../store/store'
 
@@ -31,16 +31,28 @@ import { PresetActions } from './preset-actions'
 import { useParams } from 'next/navigation'
 import { Flow } from '@prisma/client'
 import { useQuery } from '@tanstack/react-query'
+import MindMapNode from './nodes/MindMapNode'
+import MindMapEdge from './edges/MindMapEdge'
 
 const selector = (state: RFState) => ({
   nodes: state.nodes,
   edges: state.edges,
   onNodesChange: state.onNodesChange,
   onEdgesChange: state.onEdgesChange,
-});
+  addChildNode: state.addChildNode,
+})
 
-// this places the node origin in the center of a node
-const nodeOrigin: NodeOrigin = [0.5, 0.5];
+const nodeTypes = {
+  mindmap: MindMapNode,
+}
+
+const edgeTypes = {
+  mindmap: MindMapEdge,
+}
+
+const nodeOrigin: NodeOrigin = [0.5, 0.5]
+const connectionLineStyle = { stroke: '#F6AD55', strokeWidth: 3 }
+const defaultEdgeOptions = { style: connectionLineStyle, type: 'mindmap' }
 
 export default function Flow() {
   const params = useParams()
@@ -49,7 +61,61 @@ export default function Flow() {
     queryFn: () => fetchFlowDetails(params?.companyId as string, params?.flowId as string)
   })
 
-  const { nodes, edges, onNodesChange, onEdgesChange } = useStore(selector, shallow);
+  // whenever you use multiple values, you should use shallow for making sure that the component only re-renders when one of the values change
+  const { nodes, edges, onNodesChange, onEdgesChange, addChildNode } = useStore(selector, shallow)
+  const connectingNodeId = useRef<string | null>(null)
+  const store = useStoreApi()
+  const { project } = useReactFlow()
+
+  const getChildNodePosition = (event: MouseEvent, parentNode?: Node) => {
+    const { domNode } = store.getState()
+
+    if (
+      !domNode ||
+      // we need to check if these properites exist, because when a node is not initialized yet,
+      // it doesn't have a positionAbsolute nor a width or height
+      !parentNode?.positionAbsolute ||
+      !parentNode?.width ||
+      !parentNode?.height
+    ) {
+      return
+    }
+
+    const { top, left } = domNode.getBoundingClientRect()
+
+    // we need to remove the wrapper bounds, in order to get the correct mouse position
+    const panePosition = project({
+      x: event.clientX - left,
+      y: event.clientY - top,
+    })
+
+    // we are calculating with positionAbsolute here because child nodes are positioned relative to their parent
+    return {
+      x: panePosition.x - parentNode.positionAbsolute.x + parentNode.width / 2,
+      y: panePosition.y - parentNode.positionAbsolute.y + parentNode.height / 2,
+    }
+  }
+
+  const onConnectStart: OnConnectStart = useCallback((_, { nodeId }) => {
+    connectingNodeId.current = nodeId
+  }, [])
+
+  const onConnectEnd: OnConnectEnd = useCallback(
+    (event) => {
+      const { nodeInternals } = store.getState()
+      const targetIsPane = (event.target as Element).classList.contains('react-flow__pane')
+
+      if (targetIsPane && connectingNodeId.current) {
+        const parentNode = nodeInternals.get(connectingNodeId.current)
+        const childNodePosition = getChildNodePosition(event, parentNode)
+
+        if (parentNode && childNodePosition) {
+          addChildNode(parentNode, childNodePosition)
+        }
+      }
+    },
+    [getChildNodePosition]
+  )
 
   if (isLoading) {
     return (
@@ -92,7 +158,12 @@ export default function Flow() {
                   edges={edges}
                   onNodesChange={onNodesChange}
                   onEdgesChange={onEdgesChange}
+                  nodeTypes={nodeTypes}
+                  edgeTypes={edgeTypes}
                   nodeOrigin={nodeOrigin}
+                  connectionLineStyle={connectionLineStyle}
+                  defaultEdgeOptions={defaultEdgeOptions}
+                  connectionLineType={ConnectionLineType.Straight}
                   fitView
                   className="bg-teal-50"
                 >
