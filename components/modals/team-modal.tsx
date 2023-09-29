22fa5976fc5ebfd4bb490a30e6c9e547c475019d
maina-david <waihura.m@gmail.com>
2023-09-30 00:53:40 +0300

    remove auto close on team modal

--- components/modals/team-modal.tsx
+++ components/modals/team-modal.tsx
@@ -105,7 +105,6 @@ export default function TeamModal() {
         }
       }
       teamModal.setTeam(null)
-      teamModal.onClose()
     } catch (error) {
       // Handle specific errors
       if (error.response) {
