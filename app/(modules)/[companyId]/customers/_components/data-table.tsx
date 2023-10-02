8ea6ada8039913b14aa144d2ea18f49cfe73c3fd
maina-david <waihura.m@gmail.com>
2023-10-02 23:42:58 +0300

    update buttons layout

--- app/(modules)/[companyId]/customers/_components/data-table.tsx
+++ app/(modules)/[companyId]/customers/_components/data-table.tsx
@@ -84,38 +84,40 @@ export function DataTable<TData, TValue>({
           }
           className="max-w-sm"
         />
-        <Button variant="outline" className="ml-auto">
-          <RefreshCcwIcon className="mr-2 h-4 w-4"/> Refresh
-        </Button>
-        <Button variant="outline" className="ml-auto">
-          <ImportIcon className="mr-2 h-4 w-4"/> Import
-        </Button>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="outline" className="ml-auto">
-              Columns <ChevronDownIcon className="ml-2 h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            {table
-              .getAllColumns()
-              .filter((column) => column.getCanHide())
-              .map((column) => {
-                return (
-                  <DropdownMenuCheckboxItem
-                    key={column.id}
-                    className="capitalize"
-                    checked={column.getIsVisible()}
-                    onCheckedChange={(value) =>
-                      column.toggleVisibility(!!value)
-                    }
-                  >
-                    {column.id}
-                  </DropdownMenuCheckboxItem>
-                )
-              })}
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <div className="ml-auto grid grid-flow-row-dense grid-cols-3 grid-rows-3">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline">
+                Columns <ChevronDownIcon className="ml-2 h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              {table
+                .getAllColumns()
+                .filter((column) => column.getCanHide())
+                .map((column) => {
+                  return (
+                    <DropdownMenuCheckboxItem
+                      key={column.id}
+                      className="capitalize"
+                      checked={column.getIsVisible()}
+                      onCheckedChange={(value) =>
+                        column.toggleVisibility(!!value)
+                      }
+                    >
+                      {column.id}
+                    </DropdownMenuCheckboxItem>
+                  )
+                })}
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <Button variant="outline">
+            <RefreshCcwIcon className="mr-2 h-4 w-4" /> Refresh
+          </Button>
+          <Button variant="outline">
+            <ImportIcon className="mr-2 h-4 w-4" /> Import
+          </Button>
+        </div>
       </div>
       <div className="rounded-md border">
         <Table>
