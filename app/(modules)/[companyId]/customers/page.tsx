682f9aad88d4f276445a4aa5774cc4875f86ea7a
maina-david <waihura.m@gmail.com>
2023-10-02 17:46:20 +0300

    add users datatable

--- app/(modules)/[companyId]/customers/page.tsx
+++ app/(modules)/[companyId]/customers/page.tsx
@@ -1,13 +1,48 @@
-import React from 'react'
+import * as React from "react"
 
-export const metadata = {
-  title: "Customers",
-}
+import { Payment, columns } from "./_components/columns"
+import { DataTable } from "./_components/data-table"
 
-function CustomersPage({ params }: { params: { slug: string } }) {
+const data: Payment[] = [
+  {
+    id: "m5gr84i9",
+    amount: 316,
+    status: "success",
+    email: "ken99@yahoo.com",
+  },
+  {
+    id: "3u1reuv4",
+    amount: 242,
+    status: "success",
+    email: "Abe45@gmail.com",
+  },
+  {
+    id: "derv1ws0",
+    amount: 837,
+    status: "processing",
+    email: "Monserrat44@gmail.com",
+  },
+  {
+    id: "5kma53ae",
+    amount: 874,
+    status: "success",
+    email: "Silas22@gmail.com",
+  },
+  {
+    id: "bhqecj4p",
+    amount: 721,
+    status: "failed",
+    email: "carmella@hotmail.com",
+  },
+]
+
+
+export function UsersPage() {
+
+
   return (
-    <div>CustomersPage</div>
+    <>
+      <DataTable columns={columns} data={data} />
+    </>
   )
 }
-
-export default CustomersPage
