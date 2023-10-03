7d3813422b65453d8e07b2a2038ad4923294d83b
maina-david <waihura.m@gmail.com>
2023-10-03 04:56:35 +0300

    debug onconnect

--- app/[companyId]/playground/[flowId]/components/store.ts
+++ app/[companyId]/playground/[flowId]/components/store.ts
@@ -84,13 +84,13 @@ const useFlowStore = createWithEqualityFn<RFState>((set, get) => ({
 
       set({
         edges: [...get().edges, newEdge],
-        nodes: [
-          ...get().nodes,
-          {
-            ...targetNode,
-            parentNode: sourceNode.id,
-          },
-        ],
+        // nodes: [
+        //   ...get().nodes,
+        //   {
+        //     ...targetNode,
+        //     parentNode: sourceNode.id,
+        //   },
+        // ],
       })
     }
   },
