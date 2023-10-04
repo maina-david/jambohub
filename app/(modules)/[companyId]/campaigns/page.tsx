7e1485f1fb27ec20f094de0d25dcba80615a4d27
maina-david <waihura.m@gmail.com>
2023-10-04 16:31:51 +0300

    add calendar on campaign page

--- app/(modules)/[companyId]/campaigns/page.tsx
+++ app/(modules)/[companyId]/campaigns/page.tsx
@@ -1,24 +1,84 @@
+"use client"
+
+import * as React from "react"
+import { CalendarIcon } from "@radix-ui/react-icons"
+import { addDays, format } from "date-fns"
+import { DateRange } from "react-day-picker"
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Calendar } from "@/components/ui/calendar"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+
 import { AppShell } from '@/components/shell'
-import React from 'react'
 import CampaignDialog from './_components/campaign-dialog'
 import CampaignCharts from './_components/campaign-charts'
 import { Search } from '@/components/search'
-import { Button } from '@/components/ui/button'
+import { Separator } from '@/components/ui/separator'
+import { Switch } from '@/components/ui/switch'
+import { Label } from '@/components/ui/label'
 
-export const metadata = {
-  title: "Campaigns",
-}
 
 export default function CampaignsPage() {
+  const [date, setDate] = React.useState<DateRange | undefined>({
+    from: new Date(2022, 0, 20),
+    to: addDays(new Date(2022, 0, 20), 20),
+  })
   return (
     <AppShell>
-      <CampaignDialog />
       <div className="flex items-center justify-between px-2">
-        <Search />
-        <Button className='ghost'>
-          Search
-        </Button>
+        <div className="grid gap-1">
+          <Search/>
+          <CampaignDialog/>
+        </div>
+        <div className="flex items-center space-x-2">
+          <Switch id="charts-on" />
+          <Label htmlFor="charts-on">Charts on</Label>
+        </div>
+        <div className="grid gap-2">
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button
+                id="date"
+                variant={"outline"}
+                className={cn(
+                  "w-[300px] justify-start text-left font-normal",
+                  !date && "text-muted-foreground"
+                )}
+              >
+                <CalendarIcon className="mr-2 h-4 w-4" />
+                {date?.from ? (
+                  date.to ? (
+                    <>
+                      {format(date.from, "LLL dd, y")} -{" "}
+                      {format(date.to, "LLL dd, y")}
+                    </>
+                  ) : (
+                    format(date.from, "LLL dd, y")
+                  )
+                ) : (
+                  <span>Pick a date</span>
+                )}
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0" align="start">
+              <Calendar
+                initialFocus
+                mode="range"
+                defaultMonth={date?.from}
+                selected={date}
+                onSelect={setDate}
+                numberOfMonths={2}
+              />
+            </PopoverContent>
+          </Popover>
+        </div>
       </div>
+      <Separator />
       <CampaignCharts />
     </AppShell>
   )
