d52e17181472366aa3f44e8329b30db8b50e4bbd
maina-david <waihura.m@gmail.com>
2023-10-04 13:16:27 +0300

    update dialog

--- app/(modules)/[companyId]/campaigns/_components/campaign-dialog.tsx
+++ app/(modules)/[companyId]/campaigns/_components/campaign-dialog.tsx
@@ -5,50 +5,50 @@ import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } f
 import Link from "next/link"
 
 export default function CampaignDialog() {
-    return (
-        <Dialog defaultOpen={true}>
-        <DialogContent className="w-3/4">
-            <DialogHeader>
-                <DialogTitle>
-                    Choose Campaign Source
-                </DialogTitle>
-                <DialogDescription>
-                    Select whether you want to run campaigns using your own customers or the platform&apos;s enrolled customers.
-                </DialogDescription>
-            </DialogHeader>
-                <div className="grid h-full grid-cols-2 gap-2">
-                    <Link href="#">
-                        <Card className="cursor-pointer shadow-md">
-                            <CardHeader>
-                                <CardTitle>
-                                    Use Your Customers
-                                </CardTitle>
-                                <CardDescription>
-                                    Run campaigns targeting your own customers who have linked their social media accounts.
-                                </CardDescription>
-                            </CardHeader>
-                            <CardContent>
+  return (
+    <Dialog defaultOpen={true}>
+      <DialogContent className="w-full max-w-screen-md">
+        <DialogHeader>
+          <DialogTitle>
+            Choose Campaign Source
+          </DialogTitle>
+          <DialogDescription>
+            Select whether you want to run campaigns using your own customers or the platform&apos;s enrolled customers.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid grid-cols-2 gap-2">
+          <Link href="#">
+            <Card className="h-full cursor-pointer shadow-md">
+              <CardHeader>
+                <CardTitle>
+                  Use Your Customers
+                </CardTitle>
+                <CardDescription>
+                  Run targeted campaigns based on your customers&apos; communication channels (e.g., social media accounts).
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
 
-                            </CardContent>
-                        </Card>
-                    </Link>
-                    <Link href="#">
-                        <Card className="cursor-pointer shadow-md">
-                            <CardHeader>
-                                <CardTitle>
-                                    Use Platform&apos;s Enrolled Customers
-                                </CardTitle>
-                                <CardDescription>
-                                    Utilize the platform&apos;s enrolled customers who have linked their social media accounts for your campaigns.
-                                </CardDescription>
-                            </CardHeader>
-                            <CardContent>
+              </CardContent>
+            </Card>
+          </Link>
+          <Link href="#">
+            <Card className="h-full cursor-pointer shadow-md">
+              <CardHeader>
+                <CardTitle>
+                  Use Platform&apos;s Enrolled Customers
+                </CardTitle>
+                <CardDescription>
+                  Amplify your reach by leveraging the platform&apos;s registered customers for broader campaign distribution.
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
 
-                            </CardContent>
-                        </Card>
-                    </Link>
-                </div>
-            </DialogContent>
-        </Dialog>
-    )
+              </CardContent>
+            </Card>
+          </Link>
+        </div>
+      </DialogContent>
+    </Dialog>
+  )
 }
