9b1671dcca06c428b8ed1b0e183df0dc297a0621
maina-david <waihura.m@gmail.com>
2023-10-04 23:19:44 +0300

    update header

--- app/(modules)/[companyId]/campaigns/page.tsx
+++ app/(modules)/[companyId]/campaigns/page.tsx
@@ -30,52 +30,54 @@ export default function CampaignsPage() {
   return (
     <>
       <div className="flex items-start justify-between space-y-4">
-        <div className="grid gap-2">
-          <Popover>
-            <PopoverTrigger asChild>
-              <Button
-                id="date"
-                variant={"outline"}
-                className={cn(
-                  "w-[300px] justify-start text-left font-normal",
-                  !date && "text-muted-foreground"
-                )}
-              >
-                <CalendarIcon className="mr-2 h-4 w-4" />
-                {date?.from ? (
-                  date.to ? (
-                    <>
-                      {format(date.from, "LLL dd, y")} -{" "}
-                      {format(date.to, "LLL dd, y")}
-                    </>
+        <div className="flex-1 gap-4">
+          <div className="grid gap-2">
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button
+                  id="date"
+                  variant={"outline"}
+                  className={cn(
+                    "w-[300px] justify-start text-left font-normal",
+                    !date && "text-muted-foreground"
+                  )}
+                >
+                  <CalendarIcon className="mr-2 h-4 w-4" />
+                  {date?.from ? (
+                    date.to ? (
+                      <>
+                        {format(date.from, "LLL dd, y")} -{" "}
+                        {format(date.to, "LLL dd, y")}
+                      </>
+                    ) : (
+                      format(date.from, "LLL dd, y")
+                    )
                   ) : (
-                    format(date.from, "LLL dd, y")
-                  )
-                ) : (
-                  <span>Pick a date</span>
-                )}
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-auto p-0" align="start">
-              <Calendar
-                initialFocus
-                mode="range"
-                defaultMonth={date?.from}
-                selected={date}
-                onSelect={setDate}
-                numberOfMonths={2}
-              />
-            </PopoverContent>
-          </Popover>
+                    <span>Pick a date</span>
+                  )}
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className="w-auto p-0" align="start">
+                <Calendar
+                  initialFocus
+                  mode="range"
+                  defaultMonth={date?.from}
+                  selected={date}
+                  onSelect={setDate}
+                  numberOfMonths={2}
+                />
+              </PopoverContent>
+            </Popover>
+          </div>
+          <div className="grid items-center">
+            <Switch
+              id="charts-on"
+              checked={isShowingCharts}
+              onCheckedChange={setIsShowingCharts} />
+            <Label htmlFor="charts-on">Display charts</Label>
+          </div>
         </div>
-        <div className="grid items-center">
-          <Switch
-            id="charts-on"
-            checked={isShowingCharts}
-            onCheckedChange={setIsShowingCharts} />
-          <Label htmlFor="charts-on">Display charts</Label>
-        </div>
-        <div className="ml-auto flex space-x-2">
+        <div className="ml-auto">
           <CampaignDialog />
         </div>
       </div>
