c6b3e316ee9f2aeaad51048ae2f59c5b8fb80389
maina-david <waihura.m@gmail.com>
2023-10-04 15:39:07 +0300

    update chart components

--- app/(modules)/[companyId]/campaigns/page.tsx
+++ app/(modules)/[companyId]/campaigns/page.tsx
@@ -2,6 +2,8 @@ import { AppShell } from '@/components/shell'
 import React from 'react'
 import CampaignDialog from './_components/campaign-dialog'
 import CampaignCharts from './_components/campaign-charts'
+import { Search } from '@/components/search'
+import { Button } from '@/components/ui/button'
 
 export const metadata = {
   title: "Campaigns",
@@ -11,6 +13,12 @@ export default function CampaignsPage() {
   return (
     <AppShell>
       <CampaignDialog />
+      <div className="flex items-center justify-between px-2">
+        <Search />
+        <Button className='ghost'>
+          Search
+        </Button>
+      </div>
       <CampaignCharts />
     </AppShell>
   )
