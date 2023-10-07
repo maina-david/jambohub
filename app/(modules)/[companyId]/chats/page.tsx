26a373c04bd6149696f3655c07683e915499f500
maina-david <waihura.m@gmail.com>
2023-10-07 12:30:12 +0300

    start chat page

--- app/(modules)/[companyId]/chats/page.tsx
+++ app/(modules)/[companyId]/chats/page.tsx
@@ -1,12 +1,41 @@
+import { Input } from '@/components/ui/input'
+import { ScrollArea } from '@/components/ui/scroll-area'
+import { MagnifyingGlassIcon } from '@radix-ui/react-icons'
 import React from 'react'
 
 export const metadata = {
   title: "Chats",
 }
 
-function ChatsPage({ params }: { params: { slug: string } }) {
+function ChatsPage() {
   return (
-    <div>ChatsPage</div>
+    <div className='relative flex w-full border-spacing-1 overflow-hidden'>
+      {/* Chat Sidebar */}
+      <div>
+        <div className='flex items-center space-x-2'>
+          <Input
+            type="search"
+            placeholder="Search customers..."
+            className="h-8 w-full sm:w-64 sm:pr-12"
+          />
+          <kbd className="pointer-events-none absolute right-1.5 top-1.5 hidden h-5 select-none items-center gap-1 rounded border bg-background px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100 sm:flex">
+            <MagnifyingGlassIcon className='h-4 w-4' />
+          </kbd>
+        </div>
+        <ScrollArea className='h-64 w-full'>
+          <div>Chat 1</div>
+          <div>Chat 1</div>
+          <div>Chat 1</div>
+          <div>Chat 1</div>
+          <div>Chat 1</div>
+          <div>Chat 1</div>
+        </ScrollArea>
+      </div>
+      {/* Chat Content Area */}
+      <div>
+        Chat Area content
+      </div>
+    </div>
   )
 }
 
