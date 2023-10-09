733acde9e736f4582527e512bd8d5d40ab709c56
maina-david <waihura.m@gmail.com>
2023-10-09 13:32:22 +0300

    update chat page

--- app/(modules)/[companyId]/chats/page.tsx
+++ app/(modules)/[companyId]/chats/page.tsx
@@ -1,4 +1,5 @@
-import Image from 'next/image'
+import { Input } from '@/components/ui/input'
+import { ScrollArea } from '@/components/ui/scroll-area'
 import React from 'react'
 
 export const metadata = {
@@ -7,17 +8,50 @@ export const metadata = {
 
 function ChatsPage() {
   return (
-    <div className="container mx-auto rounded-lg shadow-lg">
-      <div className="flex flex-row justify-between">
-        {/* left sidebar */}
-        <div className="flex w-2/5 flex-col overflow-y-auto border-r-2">
+    <div className='relative flex w-full overflow-hidden border'>
+      {/* Sidebar Left */}
+      <div className="absolute block h-full w-[360px] flex-col border">
+        <div className='flex items-center px-5 py-3'>
+          <Input placeholder='Search for contact...' className='w-full' />
         </div>
-        {/* left sidebar */}
-        {/* chat content */}
-        <div className="w-2/5 border-l-2 px-5">
-        </div>
-        {/* chat content */}
+        <ScrollArea className='h-full w-full overflow-auto'>
+          <div className='gap-4'>
+            <h5 className='mb-3.5 ml-3 scroll-m-20 text-xl font-semibold tracking-tight'>
+              Chats
+            </h5>
+            <div className='mb-1'>
+              <div className="w-full items-start border px-3 py-2">
+                chat 1
+              </div>
+              <div className="w-full items-start border px-3 py-2">
+                chat 2
+              </div>
+              <div className="w-full items-start border px-3 py-2">
+                chat 3
+              </div>
+            </div>
+            <h5 className='mb-3.5 ml-3 scroll-m-20 text-xl font-semibold tracking-tight'>
+              Contacts
+            </h5>
+            <div className='mb-1'>
+              <div className="w-full items-start border px-3 py-2">
+                Contact 1
+              </div>
+              <div className="w-full items-start border px-3 py-2">
+                Contact 2
+              </div>
+              <div className="w-full items-start border px-3 py-2">
+                Contact 3
+              </div>
+            </div>
+          </div>
+        </ScrollArea>
       </div>
+      {/* Sidebar Left*/}
+
+      {/* ChatContent */}
+
+      {/* ChatContent */}
     </div>
   )
 }
