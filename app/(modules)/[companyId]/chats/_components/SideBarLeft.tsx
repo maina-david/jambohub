546f3ff369c642ad4ed7fe6207b7da9f06e0cc79
maina-david <waihura.m@gmail.com>
2023-10-10 23:10:32 +0300

    debug sidebar

--- app/(modules)/[companyId]/chats/_components/SideBarLeft.tsx
+++ app/(modules)/[companyId]/chats/_components/SideBarLeft.tsx
@@ -6,35 +6,40 @@ import { ScrollArea } from '@/components/ui/scroll-area'
 import { UserAvatar } from '@/components/user-avatar'
 import { Separator } from '@/components/ui/separator'
 import useChatStore, { ChatState } from '@/store/chat'
+import { User } from 'next-auth'
+import { Chat, Contact } from '@prisma/client'
 
-const selector = (state: ChatState) => ({
-  chats: state.chats,
-  contacts: state.contacts,
-  userProfile: state.userProfile,
-  setSelectedChat: state.setSelectedChat,
-  setUserProfile: state.setUserProfile,
-})
+// const selector = (state: ChatState) => ({
+//   chats: state.chats,
+//   contacts: state.contacts,
+//   userProfile: state.userProfile,
+//   setSelectedChat: state.setSelectedChat,
+//   setUserProfile: state.setUserProfile,
+// })
 
 export default function SideBarLeft() {
-  const {
-    chats,
-    contacts,
-    userProfile,
-    setSelectedChat,
-    setUserProfile,
-  } = useChatStore(selector)
+  // const {
+  //   chats,
+  //   contacts,
+  //   userProfile,
+  //   setSelectedChat,
+  //   setUserProfile,
+  // } = useChatStore(selector)
 
-  useEffect(() => {
-    setUserProfile()
-  }, [setUserProfile])
+  // useEffect(() => {
+  //   setUserProfile()
+  // }, [setUserProfile])
+
+  const chats: Chat[] = []
+  const contacts: Contact[] = []
 
   return (
     <div className="flex flex-col rounded-l border md:w-1/3">
       <div className="flex items-center justify-between space-x-2 p-2.5">
-        <UserAvatar
+        {/* <UserAvatar
           user={{ name: userProfile?.name || null, image: userProfile?.image || null }}
           className="h-8 w-8"
-        />
+        /> */}
         <Input placeholder="Search for contact..." />
       </div>
       <Separator />
@@ -43,9 +48,9 @@ export default function SideBarLeft() {
         <div className="mb-5">
           {chats.map((chat) => (
             <div
-              key={chat.id} // Assuming each chat has a unique ID
+              key={chat.id}
               className="w-full cursor-pointer items-start px-3 py-2"
-              onClick={() => setSelectedChat(chat.id)}
+              // onClick={() => setSelectedChat(chat.id)}
             >
               {chat.externalRef}
             </div>
