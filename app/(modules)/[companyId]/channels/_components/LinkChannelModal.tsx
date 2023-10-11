f236bf0f9f496aceb1defeace8b51bf552abeb32
maina-david <waihura.m@gmail.com>
2023-10-12 00:27:09 +0300

    check if new form c=works

--- app/(modules)/[companyId]/channels/_components/LinkChannelModal.tsx
+++ app/(modules)/[companyId]/channels/_components/LinkChannelModal.tsx
@@ -14,16 +14,37 @@ import {
 import { cn } from '@/lib/utils'
 import { Channel } from '@prisma/client'
 import WhatsAppForm from './forms/WhatsAppForm'
+import { Form } from "@/components/ui/form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import * as z from "zod"
+import { useForm } from 'react-hook-form'
 
 interface ChannelProps extends React.HTMLAttributes<HTMLDivElement> {
   channel: Channel
 }
 
+const WhatsAppFormSchema = z.object({
+  accessToken: z.string().min(1),
+  phoneNumberId: z.number(),
+})
+
+const SMSFormSchema = z.object({
+  apiKey: z.string()
+})
+
 export default function LinkChannelModal({ channel, className, children }: ChannelProps) {
   const [isOpen, setIsOpen] = useState<boolean>(false)
 
+  const form = useForm<z.infer<typeof WhatsAppFormSchema>>({
+    resolver: zodResolver(WhatsAppFormSchema),
+    defaultValues: {
+      accessToken: "",
+      phoneNumberId: undefined,
+    },
+  })
+
   // Handle form submission
-  const handleSubmit = (data: any) => {
+  const onSubmit = (data: any) => {
     try {
       console.log(data)
     } catch (error) {
@@ -37,7 +58,7 @@ export default function LinkChannelModal({ channel, className, children }: Chann
   const renderChannelForm = () => {
     switch (channel.type) {
       case 'WHATSAPP':
-        return <WhatsAppForm onSubmit={handleSubmit} />
+        return <WhatsAppForm form={form} />
       default:
         return null
     }
@@ -56,11 +77,15 @@ export default function LinkChannelModal({ channel, className, children }: Chann
           <DialogDescription>Configure your {channel.type.toLowerCase()} settings below</DialogDescription>
         </DialogHeader>
         <div className="grid gap-4 py-4">
-          {renderChannelForm()}
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              {renderChannelForm()}
+              <DialogFooter>
+                <Button type="submit">Continue</Button>
+              </DialogFooter>
+            </form>
+          </Form>
         </div>
-        <DialogFooter>
-          <Button onClick={handleSubmit} type="submit">Continue</Button>
-        </DialogFooter>
       </DialogContent>
     </Dialog>
   )
--- app/(modules)/[companyId]/channels/_components/forms/WhatsAppForm.tsx
+++ app/(modules)/[companyId]/channels/_components/forms/WhatsAppForm.tsx
@@ -1,9 +1,7 @@
 "use client"
 
-import * as z from "zod"
 import React from 'react'
-import { useForm } from 'react-hook-form'
-import { zodResolver } from "@hookform/resolvers/zod"
+import { Control, FieldValues, useForm } from 'react-hook-form'
 import {
   Form,
   FormControl,
@@ -15,56 +13,41 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 
-export const formSchema = z.object({
-  accessToken: z.string().min(1),
-  phoneNumberId: z.number(),
-})
-
-export default function WhatsAppForm({ onSubmit }) {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      accessToken: "",
-      phoneNumberId: undefined,
-    },
-  })
-
+export default function WhatsAppForm(form) {
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="accessToken"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Access Token</FormLabel>
-              <FormControl>
-                <Input placeholder="" {...field} />
-              </FormControl>
-              <FormDescription>
-                Enter your permanent access token.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="phoneNumberId"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Phone Number ID</FormLabel>
-              <FormControl>
-                <Input placeholder="" {...field} />
-              </FormControl>
-              <FormDescription>
-                Enter your numeric phone number ID.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-      </form>
-    </Form>
+    <>
+      <FormField
+        control={form.control}
+        name="accessToken"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Access Token</FormLabel>
+            <FormControl>
+              <Input placeholder="" {...field} />
+            </FormControl>
+            <FormDescription>
+              Enter your permanent access token.
+            </FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="phoneNumberId"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Phone Number ID</FormLabel>
+            <FormControl>
+              <Input placeholder="" {...field} />
+            </FormControl>
+            <FormDescription>
+              Enter your numeric phone number ID.
+            </FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    </>
   )
 }
