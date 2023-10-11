a55e05ca5f3b0f57b8bd5d4d08f0284b5c86427c
maina-david <waihura.m@gmail.com>
2023-10-11 13:16:23 +0300

    fix whatsapp webhook verification

--- app/api/webhooks/whatsapp/route.ts
+++ app/api/webhooks/whatsapp/route.ts
@@ -1,8 +1,9 @@
 
-export async function GET(req: Request) {
-  const body = await req.json()
-  if (body.hub_challenge) {
-    return new Response(body.hub_challenge, { status: 200 })
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url)
+  const hub_challenge = searchParams.get('hub_challenge')
+  if (hub_challenge) {
+    return new Response(hub_challenge, { status: 200 })
   }
   return new Response(null, { status: 200 })
 }
