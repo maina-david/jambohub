79e99bedb10bc2bf4ba057e199416f98e1bfd12f
maina-david <waihura.m@gmail.com>
2023-10-11 12:49:02 +0300

    add whatsapp webhook verification

--- app/api/webhooks/whatsapp/route.ts
+++ app/api/webhooks/whatsapp/route.ts
@@ -1,4 +1,7 @@
 
 export async function POST(req: Request) {
-  return new Response(null, { status: 200 })
+  const body = await req.json()
+  if (body.hub_challenge) {
+    return new Response(body.hub_challenge, { status: 200 })
+  }
 }
