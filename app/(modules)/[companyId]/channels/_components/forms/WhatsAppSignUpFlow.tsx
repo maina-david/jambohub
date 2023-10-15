03a2c159436ab7d8340e72d7ce6054d0a0e6bc9e
maina-david <waihura.m@gmail.com>
2023-10-15 22:01:11 +0300

    initialize facebook sdk then start app

--- app/(modules)/[companyId]/channels/_components/forms/WhatsAppSignUpFlow.tsx
+++ app/(modules)/[companyId]/channels/_components/forms/WhatsAppSignUpFlow.tsx
@@ -1,14 +1,10 @@
 'use client'
 
-import React, { useEffect } from 'react'
-import { initializeFacebookSDK, launchWhatsAppSignup } from '@/lib/facebook'
+import React from 'react'
+import { launchWhatsAppSignup } from '@/lib/facebook'
 import { Button } from '@/components/ui/button'
 
 export default function WhatsAppSignUpFlow() {
-  useEffect(() => {
-    initializeFacebookSDK()
-
-  }, [])
 
   const handleWhatsAppSignup = () => {
     // Call the function to launch WhatsApp signup
