835f502e0d4f9b406c03500633fbeeb4d091bca3
maina-david <waihura.m@gmail.com>
2023-10-15 14:45:03 +0300

    update toggle icon

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -18,7 +18,7 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="sm" className="px-0">
+        <Button variant="ghost" size="sm">
           <Icons.sun className="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Icons.moon className="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
