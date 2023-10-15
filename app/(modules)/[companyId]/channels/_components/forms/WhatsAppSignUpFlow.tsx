46f73675c2f0bdb9040a8f97f7ce2f8fa1fcbed1
maina-david <waihura.m@gmail.com>
2023-10-15 22:14:18 +0300

    update initialize facebook sdk then start app

--- app/(modules)/[companyId]/channels/_components/forms/WhatsAppSignUpFlow.tsx
+++ app/(modules)/[companyId]/channels/_components/forms/WhatsAppSignUpFlow.tsx
@@ -1,10 +1,14 @@
 'use client'
 
-import React from 'react'
-import { launchWhatsAppSignup } from '@/lib/facebook'
+import React, { useEffect } from 'react'
+import { initializeFacebookSDK, launchWhatsAppSignup } from '@/lib/facebook'
 import { Button } from '@/components/ui/button'
 
 export default function WhatsAppSignUpFlow() {
+
+  useEffect(() => {
+    initializeFacebookSDK()
+  }, [])
 
   const handleWhatsAppSignup = () => {
     // Call the function to launch WhatsApp signup
--- app/(modules)/[companyId]/layout.tsx
+++ app/(modules)/[companyId]/layout.tsx
@@ -7,14 +7,11 @@ import {
 } from "@/lib/session"
 
 import { AppLayout } from "@/components/app-layout"
-import { initializeFacebookSDK } from "@/lib/facebook"
 
 interface AppLayoutProps {
   params: { companyId: string }
   children: React.ReactNode
 }
-
-initializeFacebookSDK().then(CompanyLayout)
 
 export default async function CompanyLayout({ params, children }: AppLayoutProps) {
   const user = await getCurrentUser()
