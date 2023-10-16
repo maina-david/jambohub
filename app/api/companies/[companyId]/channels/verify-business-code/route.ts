c959cb86da0e7ba260698f79ee15ddc34cb7915d
maina-david <waihura.m@gmail.com>
2023-10-17 02:14:25 +0300

    update internal api handler

--- app/api/companies/[companyId]/channels/verify-business-code/route.ts
+++ app/api/companies/[companyId]/channels/verify-business-code/route.ts
@@ -5,39 +5,84 @@ import { authOptions } from "@/lib/auth"
 
 const routeContextSchema = z.object({
   params: z.object({
-    companyId: z.string(),
-  }),
+    companyId: z.string()
+  })
 })
 
-export async function GET(request: Request, context: z.infer<typeof routeContextSchema>) {
+async function exchangeCodeForAccessToken(code: string): Promise<{ access_token: string }> {
+  try {
+    const appId = env.NEXT_PUBLIC_FACEBOOK_APP_ID
+    const appSecret = env.FACEBOOK_APP_SECRET
+
+    const res = await fetch(`https://graph.facebook.com/v18.0/oauth/access_token?client_id=${appId}&client_secret=${appSecret}&code=${code}`, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    if (!res.ok) {
+      throw new Error(`Failed to exchange code for access token: ${res.statusText}`)
+    }
+
+    return await res.json()
+  } catch (error) {
+    throw new Error(`Error in exchangeCodeForAccessToken: ${error.message}`)
+  }
+}
+
+async function inspectAccessToken(inputToken: string): Promise<{ app_id: number; user_id: string; scopes: string[] } | null> {
+  try {
+    const appTokenOrAdminToken = env.FACEBOOK_APP_SECRET
+
+    const inspectionRes = await fetch(`https://graph.facebook.com/debug_token?input_token=${inputToken}&access_token=${appTokenOrAdminToken}`)
+
+    if (!inspectionRes.ok) {
+      throw new Error(`Failed to inspect access token: ${inspectionRes.statusText}`)
+    }
+
+    const inspectionData = await inspectionRes.json()
+
+    if (inspectionData.data) {
+      const { app_id, user_id, scopes } = inspectionData.data
+      return { app_id, user_id, scopes }
+    }
+
+    return null
+  } catch (error) {
+    throw new Error(`Error in inspectAccessToken: ${error.message}`)
+  }
+}
+
+
+export async function GET(request: Request, context: z.infer<typeof routeContextSchema>): Promise<Response> {
   try {
     const session = await getServerSession(authOptions)
 
     if (!session) {
       return new Response("Unauthorized", { status: 403 })
     }
+
     // Validate the route params.
     const { params } = routeContextSchema.parse(context)
 
     const { searchParams } = new URL(request.url)
     const code = searchParams.get('code')
 
-    const appId = env.NEXT_PUBLIC_FACEBOOK_APP_ID
-    const appSecret = env.FACEBOOK_APP_SECRET
-    const response = await fetch(`https://graph.facebook.com/v18.0/oauth/access_token?client_id=${appId}&client_secret=${appSecret}&code=${code}`, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-
-    const data: { access_token: string } = await response.json()
-
-    if (!response.ok) {
-      return new Response("Failed to verify code", { status: 422 })
+    if (!code) {
+      return new Response("Missing 'code' parameter", { status: 400 })
     }
 
-    return new Response(JSON.stringify(data.access_token), { status: 200 })
-  } catch (error) {
+    // Exchange the code for an access token.
+    const facebookResponse = await exchangeCodeForAccessToken(code)
 
+    // Now, inspect the access token using Facebook's Graph API.
+    const inputToken = facebookResponse.access_token
+
+    const inspectionResponse = await inspectAccessToken(inputToken)
+
+    return new Response(JSON.stringify(inspectionResponse), { status: 200, headers: { 'Content-Type': 'application/json' } })
+  } catch (error) {
+    // Handle errors here
+    return new Response(`An error occurred: ${error.message}`, { status: 500 })
   }
 }
