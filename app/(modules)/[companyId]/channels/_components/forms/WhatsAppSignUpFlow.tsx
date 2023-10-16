2c41e93631e6bcde3e5960d83b4d31c8e4a8f565
maina-david <waihura.m@gmail.com>
2023-10-16 18:38:50 +0300

    update facebook config

--- app/(modules)/[companyId]/channels/_components/forms/WhatsAppSignUpFlow.tsx
+++ app/(modules)/[companyId]/channels/_components/forms/WhatsAppSignUpFlow.tsx
@@ -16,18 +16,9 @@ export default function WhatsAppSignUpFlow() {
         autoLogAppEvents: true,
         xfbml: true,
         version: 'v18.0'
-      });
-    };
-    (function (d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) { return; }
-      js = d.createElement(s); js.id = id;
-      js.src = "https://connect.facebook.net/zh_TW/all.js";
-      if (fjs && fjs.parentNode) {
-        fjs.parentNode.insertBefore(js, fjs);
-      }
-    }(document, 'script', 'facebook-jssdk'));
-  }, []);
+      })
+    }
+  }, [])
 
   const handleWhatsAppSignup = () => {
     window.FB.login(function (response) {
