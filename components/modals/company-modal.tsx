739e9a5159ccd4c7025e629d2ff6658cebaad8d0
maina-david <waihura.m@gmail.com>
2023-10-16 16:34:03 +0300

    update company dialog

--- components/modals/company-modal.tsx
+++ components/modals/company-modal.tsx
@@ -16,6 +16,7 @@ import { Input } from "@/components/ui/input"
 import { Icons } from "../icons"
 import { ToastAction } from "../ui/toast"
 import { companySchema } from "@/lib/validations/company"
+import { ScrollArea } from "../ui/scroll-area"
 
 export const CompanyModal = () => {
   const companyModal = useCompanyModal()
@@ -101,178 +102,180 @@ export const CompanyModal = () => {
 
   return (
     <Dialog open={companyModal.isOpen} onOpenChange={onChange}>
-      <DialogContent className="min-w-full">
+      <DialogContent className="md:min-w-[75%]">
         <DialogHeader>
           <DialogTitle>Create company</DialogTitle>
           <DialogDescription>
             Add a new company to manage channels, teams, flows, and campaigns.
           </DialogDescription>
         </DialogHeader>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="md:flex md:flex-wrap">
-            {/* Name and Email */}
-            <div className="md:w-1/2 md:pr-2">
-              <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Company&apos;s Name</FormLabel>
-                    <FormControl>
-                      <Input disabled={loading} placeholder="Enter company name" {...field} />
-                    </FormControl>
-                    <FormDescription>This will be your company&apos;s display name.</FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-            <div className="md:w-1/2 md:pl-2">
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Company&apos;s Email</FormLabel>
-                    <FormControl>
-                      <Input disabled={loading} placeholder="Enter company's email" {...field} />
-                    </FormControl>
-                    <FormDescription>Enter the email address for your company.</FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
+        <ScrollArea>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="md:flex md:flex-wrap">
+              {/* Name and Email */}
+              <div className="md:w-1/2 md:pr-2">
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Company&apos;s Name</FormLabel>
+                      <FormControl>
+                        <Input disabled={loading} placeholder="Enter company name" {...field} />
+                      </FormControl>
+                      <FormDescription>This will be your company&apos;s display name.</FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="md:w-1/2 md:pl-2">
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Company&apos;s Email</FormLabel>
+                      <FormControl>
+                        <Input disabled={loading} placeholder="Enter company's email" {...field} />
+                      </FormControl>
+                      <FormDescription>Enter the email address for your company.</FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
 
-            {/* Phone and Website */}
-            <div className="md:w-1/2 md:pr-2">
-              <FormField
-                control={form.control}
-                name="phone"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Phone Number</FormLabel>
-                    <FormControl>
-                      <Input disabled={loading} placeholder="Enter phone number" {...field} />
-                    </FormControl>
-                    <FormDescription>Enter the phone number for your company.</FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-            <div className="md:w-1/2 md:pl-2">
-              <FormField
-                control={form.control}
-                name="website"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Company&apos;s Website</FormLabel>
-                    <FormControl>
-                      <Input disabled={loading} placeholder="Enter company's website" {...field} />
-                    </FormControl>
-                    <FormDescription>Enter the website URL for your company.</FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
+              {/* Phone and Website */}
+              <div className="md:w-1/2 md:pr-2">
+                <FormField
+                  control={form.control}
+                  name="phone"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Phone Number</FormLabel>
+                      <FormControl>
+                        <Input disabled={loading} placeholder="Enter phone number" {...field} />
+                      </FormControl>
+                      <FormDescription>Enter the phone number for your company.</FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="md:w-1/2 md:pl-2">
+                <FormField
+                  control={form.control}
+                  name="website"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Company&apos;s Website</FormLabel>
+                      <FormControl>
+                        <Input disabled={loading} placeholder="Enter company's website" {...field} />
+                      </FormControl>
+                      <FormDescription>Enter the website URL for your company.</FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
 
-            {/* Street Address */}
-            <div className="w-full">
-              <FormField
-                control={form.control}
-                name="streetAddress"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Street Address</FormLabel>
-                    <FormControl>
-                      <Input disabled={loading} placeholder="Enter street address" {...field} />
-                    </FormControl>
-                    <FormDescription>Enter the street address for your company.</FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
+              {/* Street Address */}
+              <div className="w-full">
+                <FormField
+                  control={form.control}
+                  name="streetAddress"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Street Address</FormLabel>
+                      <FormControl>
+                        <Input disabled={loading} placeholder="Enter street address" {...field} />
+                      </FormControl>
+                      <FormDescription>Enter the street address for your company.</FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
 
-            {/* City, State, and ZIP Code */}
-            <div className="md:w-1/3 md:pr-2">
-              <FormField
-                control={form.control}
-                name="city"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>City</FormLabel>
-                    <FormControl>
-                      <Input disabled={loading} placeholder="Enter city" {...field} />
-                    </FormControl>
-                    <FormDescription>Enter the city where your company is located.</FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-            <div className="md:w-1/3 md:px-2">
-              <FormField
-                control={form.control}
-                name="state"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>State</FormLabel>
-                    <FormControl>
-                      <Input disabled={loading} placeholder="Enter state" {...field} />
-                    </FormControl>
-                    <FormDescription>Enter the state where your company is located.</FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-            <div className="md:w-1/3 md:pl-2">
-              <FormField
-                control={form.control}
-                name="zipCode"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Zip Code</FormLabel>
-                    <FormControl>
-                      <Input disabled={loading} placeholder="Enter zip code" {...field} />
-                    </FormControl>
-                    <FormDescription>Enter the ZIP code for your company&apos;s location.</FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
+              {/* City, State, and ZIP Code */}
+              <div className="md:w-1/3 md:pr-2">
+                <FormField
+                  control={form.control}
+                  name="city"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>City</FormLabel>
+                      <FormControl>
+                        <Input disabled={loading} placeholder="Enter city" {...field} />
+                      </FormControl>
+                      <FormDescription>Enter the city where your company is located.</FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="md:w-1/3 md:px-2">
+                <FormField
+                  control={form.control}
+                  name="state"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>State</FormLabel>
+                      <FormControl>
+                        <Input disabled={loading} placeholder="Enter state" {...field} />
+                      </FormControl>
+                      <FormDescription>Enter the state where your company is located.</FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="md:w-1/3 md:pl-2">
+                <FormField
+                  control={form.control}
+                  name="zipCode"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Zip Code</FormLabel>
+                      <FormControl>
+                        <Input disabled={loading} placeholder="Enter zip code" {...field} />
+                      </FormControl>
+                      <FormDescription>Enter the ZIP code for your company&apos;s location.</FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
 
-            {/* Country */}
-            <div className="w-full">
-              <FormField
-                control={form.control}
-                name="country"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Country</FormLabel>
-                    <FormControl>
-                      <Input disabled={loading} placeholder="Enter country" {...field} />
-                    </FormControl>
-                    <FormDescription>Enter the country where your company is located.</FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
+              {/* Country */}
+              <div className="w-full">
+                <FormField
+                  control={form.control}
+                  name="country"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Country</FormLabel>
+                      <FormControl>
+                        <Input disabled={loading} placeholder="Enter country" {...field} />
+                      </FormControl>
+                      <FormDescription>Enter the country where your company is located.</FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
 
-            <DialogFooter>
-              <Button disabled={loading} type="submit">
-                {loading && (
-                  <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-                )}{" "}Continue
-              </Button>
-            </DialogFooter>
-          </form>
-        </Form>
+              <DialogFooter>
+                <Button disabled={loading} type="submit">
+                  {loading && (
+                    <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                  )}{" "}Continue
+                </Button>
+              </DialogFooter>
+            </form>
+          </Form>
+        </ScrollArea>
       </DialogContent>
     </Dialog>
   )
