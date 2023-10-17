93252b5bd7e735db391b07078104cd14a2750429
maina-david <waihura.m@gmail.com>
2023-10-17 21:53:48 +0300

    update facebook config

--- lib/facebook.ts
+++ lib/facebook.ts
@@ -12,7 +12,17 @@ export const initializeFaceBookSDK = (): Promise<void> => {
           cookie: true,
           xfbml: true,
           version: 'v18.0'
-        })
+        });
+        
+        (function (d, s: string, id: string) {
+          var js, fjs = d.getElementsByTagName(s)[0]
+          if (d.getElementById(id)) return
+          js = d.createElement(s)
+          js.id = id
+          js.src = "https://connect.facebook.net/en_US/sdk.js"
+          if (fjs && fjs.parentNode) fjs.parentNode.insertBefore(js, fjs)
+        }(document, 'script', 'facebook-jssdk'))
+
         resolve()
       } catch (error) {
         reject(error)
