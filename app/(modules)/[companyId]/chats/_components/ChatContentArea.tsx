e6d388d7ba12fbf3eac2344c55078c6c62392bf1
maina-david <waihura.m@gmail.com>
2023-10-21 16:49:29 +0300

    update chat area

--- app/(modules)/[companyId]/chats/_components/ChatContentArea.tsx
+++ app/(modules)/[companyId]/chats/_components/ChatContentArea.tsx
@@ -7,49 +7,29 @@ import { Input } from '@/components/ui/input'
 import { ScrollArea } from '@/components/ui/scroll-area'
 import { Separator } from '@/components/ui/separator'
 import { PaperPlaneIcon } from '@radix-ui/react-icons'
-import { Check, CheckIcon, MenuIcon, PhoneCallIcon, SearchIcon, VideoIcon, XCircleIcon } from 'lucide-react'
+import { CheckIcon, XCircleIcon, MenuIcon, PhoneCallIcon, SearchIcon, VideoIcon } from 'lucide-react'
 import { Icons } from '@/components/icons'
 import { cn } from '@/lib/utils'
-import EmojiPicker, {
-  EmojiStyle,
-  SkinTones,
-  Theme,
-  Categories,
-  EmojiClickData,
-  Emoji,
-  SuggestionMode,
-  SkinTonePickerLocation
-} from "emoji-picker-react"
-import {
-  Dialog,
-  DialogContent,
-  DialogTrigger,
-} from "@/components/ui/dialog"
-import { SmilePlusIcon } from "lucide-react"
+import EmojiPicker, { EmojiClickData, EmojiStyle } from 'emoji-picker-react'
+import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog'
+import { SmilePlusIcon } from 'lucide-react'
 import { ChatProps } from '@/types/chat-types'
 import { toast } from '@/components/ui/use-toast'
 import { useQueryClient } from '@tanstack/react-query'
 
 interface ChatContentAreaProps {
-  hidden: boolean,
+  hidden: boolean
   isMdAndAbove: boolean
   handleLeftSidebarToggle: () => void
   selectedChat: ChatProps | null
 }
 
-const ChatContentArea = (props: ChatContentAreaProps) => {
-  const {
-    hidden,
-    isMdAndAbove,
-    handleLeftSidebarToggle,
-    selectedChat
-  } = props
+const ChatContentArea: React.FC<ChatContentAreaProps> = (props) => {
+  const { hidden, isMdAndAbove, handleLeftSidebarToggle, selectedChat } = props
   const queryClient = useQueryClient()
   const [isSending, setIsSending] = useState<boolean>(false)
-  // ** Ref
   const chatArea = useRef(null)
 
-  // Function to scroll to the bottom of the chat area
   const scrollToBottom = () => {
     if (chatArea.current) {
       if (hidden) {
@@ -66,8 +46,8 @@ const ChatContentArea = (props: ChatContentAreaProps) => {
     if (selectedChat && selectedChat.chatMessages && selectedChat.chatMessages.length) {
       scrollToBottom()
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [selectedChat])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedChat, hidden])
 
   const handleStartConversation = () => {
     if (!isMdAndAbove) {
@@ -77,11 +57,8 @@ const ChatContentArea = (props: ChatContentAreaProps) => {
 
   const [message, setMessage] = useState<string>('')
 
-  function onClick(emojiData: EmojiClickData, event: MouseEvent) {
-    setMessage(
-      (message: string) =>
-        message + (emojiData.emoji)
-    )
+  function onClick(emojiData: EmojiClickData) {
+    setMessage((prevMessage) => prevMessage + emojiData.emoji)
   }
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -95,16 +72,16 @@ const ChatContentArea = (props: ChatContentAreaProps) => {
         const response = await axios.post('/api/chats/send-message', {
           chatId: selectedChat.id,
           messageType: 'TEXT',
-          message
+          message,
         })
         queryClient.invalidateQueries({ queryKey: ['assignedChats'] })
         setMessage('')
       }
     } catch (error) {
-      console.log("Error sending message: ", error.message)
+      console.error('Error sending message:', error.message)
       toast({
         title: 'Error',
-        description: 'Error sending message. Try again'
+        description: 'Error sending message. Try again',
       })
     } finally {
       setIsSending(false)
@@ -119,19 +96,20 @@ const ChatContentArea = (props: ChatContentAreaProps) => {
   }
 
   return (
-    <div className={cn("whatsapp-chat-container flex flex-col", isMdAndAbove ? 'w-2/3' : 'grow')}>
+    <div className={cn('whatsapp-chat-container flex flex-col', isMdAndAbove ? 'w-2/3' : 'grow')}>
       {selectedChat ? (
         <>
-          {/* Chat header */}
           <div className="flex items-center justify-between px-5 py-2.5">
-            {isMdAndAbove ? null : (
-              <div className='mx-2' onClick={handleLeftSidebarToggle}>
+            {!isMdAndAbove && (
+              <div className="mx-2" onClick={handleLeftSidebarToggle}>
                 <MenuIcon className="h-4 w-4" />
               </div>
             )}
             <div className="flex cursor-pointer items-center">
               <div className="flex flex-col">
-                <h6 className="scroll-m-20 text-xl font-semibold tracking-tight">{selectedChat.Contact.alias || selectedChat.Contact.identifier}</h6>
+                <h6 className="scroll-m-20 text-xl font-semibold tracking-tight">
+                  {selectedChat.Contact.alias || selectedChat.Contact.identifier}
+                </h6>
               </div>
             </div>
             <div className="flex items-center space-x-2">
@@ -147,15 +125,12 @@ const ChatContentArea = (props: ChatContentAreaProps) => {
             </div>
           </div>
           <Separator />
-
-          {/* Chat messages */}
           <div className="flex h-[470px] flex-col overflow-hidden">
             <ScrollArea ref={chatArea} className="flex-1 overflow-y-auto">
               {selectedChat.chatMessages?.map((chatMessage, index) => (
                 <div
                   key={index}
-                  className={cn('flex',
-                    chatMessage.userId ? 'mr-2 flex-row-reverse' : 'flex-row')}
+                  className={cn('flex', chatMessage.userId ? 'mr-2 flex-row-reverse' : 'flex-row')}
                 >
                   <div className="mb-2 max-w-[70%] p-2">
                     <div
@@ -166,10 +141,9 @@ const ChatContentArea = (props: ChatContentAreaProps) => {
                     >
                       {chatMessage.message}
                     </div>
-                    <div className={cn("flex space-x-1 text-sm text-gray-600 dark:text-gray-400",
-                      chatMessage.userId ? 'justify-end' : 'justify-start')}>
+                    <div className={cn('flex space-x-1 text-sm text-gray-600 dark:text-gray-400', chatMessage.userId ? 'justify-end' : 'justify-start')}>
                       {chatMessage.userId ? (
-                        <div className='flex'>
+                        <div className="flex">
                           {chatMessage.internalStatus === 'sent' ? (
                             <>
                               <CheckIcon className="h-4 w-4 text-green-500 dark:text-green-300" />
@@ -218,8 +192,6 @@ const ChatContentArea = (props: ChatContentAreaProps) => {
                 </div>
               ))}
             </ScrollArea>
-
-            {/* Input area */}
             <div className="flex border p-3">
               <Input
                 value={message}
@@ -229,7 +201,7 @@ const ChatContentArea = (props: ChatContentAreaProps) => {
               />
               <Dialog>
                 <DialogTrigger asChild>
-                  <Button variant="ghost" size={'icon'} className='mx-2'>
+                  <Button variant="ghost" size="icon" className="mx-2">
                     <SmilePlusIcon className="h-4 w-4" />
                   </Button>
                 </DialogTrigger>
@@ -238,14 +210,15 @@ const ChatContentArea = (props: ChatContentAreaProps) => {
                     onEmojiClick={onClick}
                     autoFocusSearch={false}
                     emojiStyle={EmojiStyle.NATIVE}
-                    width={'100%'}
+                    width="100%"
                   />
                 </DialogContent>
               </Dialog>
               <Button
                 className="ml-2"
                 disabled={!message || isSending}
-                onClick={handleSend}>
+                onClick={handleSend}
+              >
                 <PaperPlaneIcon className="mr-2 h-4 w-4" />
                 Send
               </Button>
@@ -253,7 +226,7 @@ const ChatContentArea = (props: ChatContentAreaProps) => {
           </div>
         </>
       ) : (
-        <div className={cn("flex flex-col items-center justify-center", isMdAndAbove ? 'h-[470px]' : 'h-[500px]')}>
+        <div className={cn('flex flex-col items-center justify-center', isMdAndAbove ? 'h-[470px]' : 'h-[500px]')}>
           <div className="mb-6 flex h-32 w-32 items-center justify-center rounded-full bg-muted px-7 pb-7 pt-8 shadow-2xl">
             <Icons.chat className="h-16 w-16" />
           </div>
@@ -261,9 +234,8 @@ const ChatContentArea = (props: ChatContentAreaProps) => {
             <p className="text-lg font-medium leading-normal">Start Conversation</p>
           </div>
         </div>
-      )
-      }
-    </div >
+      )}
+    </div>
   )
 }
 
