5f84b16cb8f250200dcc8647050316f0ca36c950
maina-david <waihura.m@gmail.com>
2023-10-21 18:18:29 +0300

    update get messages routes

--- actions/chat-actions.ts
+++ actions/chat-actions.ts
@@ -9,4 +9,4 @@ export const fetchAssignedChats = (companyId: string): Promise<ChatProps[]> =>
   axios.get(`/api/companies/${companyId}/chats`).then((response) => response.data)
 
 export const getSelectedChatMessages = (chatId: string): Promise<ChatMessage[]> =>
-  axios.get(`/api/chat/${chatId}/messages`).then((response) => response.data)
+  axios.get(`/api/chats/${chatId}/messages`).then((response) => response.data)
