e027a99c0521589f99e30497d4f15122ae2b652f
maina-david <waihura.m@gmail.com>
2023-10-23 00:08:02 +0300

    stringify json column and parse nodes

--- app/api/companies/[companyId]/flows/[flowId]/publish/route.ts
+++ app/api/companies/[companyId]/flows/[flowId]/publish/route.ts
@@ -35,7 +35,7 @@ export async function PATCH(req: Request, context: z.infer<typeof routeContextSc
     const body = flowPatchSchema.parse(json)
 
     // Fetch the flow data as JSON.
-    const flowData = await db.flow.findUnique({
+    const flow = await db.flow.findUnique({
       where: {
         id: params.flowId,
       },
@@ -44,61 +44,58 @@ export async function PATCH(req: Request, context: z.infer<typeof routeContextSc
       },
     })
 
-    if (flowData) {
-      if (body.published) {
-        const jsonString = typeof flowData.flowData === 'string' ? flowData.flowData : JSON.stringify(flowData.flowData)
-        const serializedData = JSON.stringify(jsonString)
+    if (flow) {
+      if (body.published && flow.flowData) {
+        const jsonString = typeof flow.flowData === 'string' ? flow.flowData : JSON.stringify(flow.flowData)
+        const { nodes, edges } = JSON.parse(jsonString)
 
-        console.log("Serialized Data: ", serializedData)
-        // Parse the JSON to access nodes and edges.
+        // Map nodes to the ConversationFlow model.
+        for (const node of nodes) {
+          const data = {
+            value: node.data.value,
+            replyOption: node.data.replyOption || null,
+          }
 
-        // // Map nodes to the ConversationFlow model.
-        // for (const node of nodes) {
-        //   const data = {
-        //     value: node.data.value,
-        //     replyOption: node.data.replyOption || null,
-        //   }
+          const conversationFlow = await db.conversationFlow.create({
+            data: {
+              nodeId: node.id,
+              parentNodeId: null,
+              childNodeId: null,
+              nodeType: node.type,
+              nodeOption: data.replyOption,
+              nodeData: data.value,
+              flowId: params.flowId,
+            },
+          })
+        }
 
-        //   const conversationFlow = await db.conversationFlow.create({
-        //     data: {
-        //       nodeId: node.id,
-        //       parentNodeId: null,
-        //       childNodeId: null,
-        //       nodeType: node.type,
-        //       nodeOption: data.replyOption,
-        //       nodeData: data.value,
-        //       flowId: params.flowId,
-        //     },
-        //   })
-        // }
+        // Map edges to link parent and child nodes.
+        for (const edge of edges) {
+          const sourceNode = nodes.find((node) => node.id === edge.source)
+          const targetNode = nodes.find((node) => node.id === edge.target)
 
-        // // Map edges to link parent and child nodes.
-        // for (const edge of edges) {
-        //   const sourceNode = nodes.find((node) => node.id === edge.source)
-        //   const targetNode = nodes.find((node) => node.id === edge.target)
+          if (sourceNode && targetNode) {
+            await db.conversationFlow.updateMany({
+              where: {
+                nodeId: sourceNode.id,
+                flowId: params.flowId,
+              },
+              data: {
+                childNodeId: targetNode.id,
+              },
+            })
+          }
+        }
 
-        //   if (sourceNode && targetNode) {
-        //     await db.conversationFlow.updateMany({
-        //       where: {
-        //         nodeId: sourceNode.id,
-        //         flowId: params.flowId,
-        //       },
-        //       data: {
-        //         childNodeId: targetNode.id,
-        //       },
-        //     })
-        //   }
-        // }
-
-        // // Update the published status in the Flow model.
-        // await db.flow.update({
-        //   where: {
-        //     id: params.flowId,
-        //   },
-        //   data: {
-        //     published: true,
-        //   },
-        // })
+        // Update the published status in the Flow model.
+        await db.flow.update({
+          where: {
+            id: params.flowId,
+          },
+          data: {
+            published: true,
+          },
+        })
 
         return new Response(null, { status: 200 })
       } else {
