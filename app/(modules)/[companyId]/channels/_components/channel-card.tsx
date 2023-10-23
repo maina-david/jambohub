c30a630378c367a10743ce85ed38c43cb0b71dce
maina-david <waihura.m@gmail.com>
2023-10-23 03:08:54 +0300

    update linking channel card

--- app/(modules)/[companyId]/channels/_components/channel-card.tsx
+++ app/(modules)/[companyId]/channels/_components/channel-card.tsx
@@ -25,7 +25,7 @@ import {
 import { useChannelModal } from "@/hooks/use-channel-modal"
 import { cn } from "@/lib/utils"
 import { Channel, Flow } from "@prisma/client"
-import { CircleEllipsisIcon, PencilIcon, Trash2Icon } from "lucide-react"
+import { CircleEllipsisIcon, Link2Icon, PencilIcon, Trash2Icon } from "lucide-react"
 import Image from "next/image"
 import { useParams } from "next/navigation"
 import { useEffect, useState } from "react"
@@ -428,7 +428,7 @@ export function ChannelCard({ channel }: ChannelProps) {
                     {isLinkingChannelFlow && (
                       <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
                     )}
-                    Link Channel To Flow
+                    Channel <Link2Icon /> Flow
                   </Button>
                 </DialogTrigger>
                 <DialogContent>
