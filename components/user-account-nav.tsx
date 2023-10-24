feb08dfb11fa8b3f4094bac46214f8d7207facb8
maina-david <waihura.m@gmail.com>
2023-10-24 18:25:19 +0300

    update theme icons

--- components/user-account-nav.tsx
+++ components/user-account-nav.tsx
@@ -16,6 +16,7 @@ import { UserAvatar } from "@/components/user-avatar"
 import { Icons } from "./icons"
 import { Label } from "./ui/label"
 import { useTheme } from "next-themes"
+import { MoonIcon, SunIcon } from "lucide-react"
 
 interface UserAccountNavProps extends React.HTMLAttributes<HTMLDivElement> {
   user: Pick<User, "name" | "image" | "email">
@@ -69,16 +70,19 @@ export function UserAccountNav({ user, mdAndAbove }: UserAccountNavProps) {
               <Switch id="theme"
                 checked={isLightTheme}
                 onCheckedChange={() => {
-                  if(isLightTheme){
+                  if (isLightTheme) {
                     setTheme('dark')
-                  }else{
+                  } else {
                     setTheme('light')
                   }
                 }}
               />
               <Label htmlFor="theme">
-                <Icons.sun className="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                <Icons.moon className="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                {isLightTheme ? (
+                  <SunIcon className="h-4 w-4" />
+                ) : (
+                  <MoonIcon className="h-4 w-4" />
+                )}
               </Label>
             </div>
           </DropdownMenuItem>
