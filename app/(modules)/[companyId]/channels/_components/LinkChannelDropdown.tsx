31d6e4ab2255e8688f2c0ad769d116d2e5642e68
maina-david <waihura.m@gmail.com>
2023-10-24 21:41:57 +0300

    add initializing loader to whatsapp

--- app/(modules)/[companyId]/channels/_components/LinkChannelDropdown.tsx
+++ app/(modules)/[companyId]/channels/_components/LinkChannelDropdown.tsx
@@ -2,9 +2,16 @@
 
 import React, { useEffect, useState } from 'react'
 import { ConfigurationId, initializeFaceBookSDK } from '@/lib/facebook'
-import { FaWhatsapp, FaXTwitter, FaFacebookMessenger, FaCommentSms } from "react-icons/fa6"
+import {
+  FaWhatsapp,
+  FaXTwitter,
+  FaFacebookMessenger,
+  FaCommentSms
+} from "react-icons/fa6"
+import { FiLoader } from "react-icons/fi"
+import { CiWarning } from "react-icons/ci"
 import { BiDialpad } from "react-icons/bi"
-import { useParams} from 'next/navigation'
+import { useParams } from 'next/navigation'
 import { toast } from "@/components/ui/use-toast"
 import axios from 'axios'
 import {
@@ -19,16 +26,21 @@ import { Button } from '@/components/ui/button'
 
 export default function LinkChannelDropdown() {
   const params = useParams()
-  const [sdkInitialized, setSdkInitialized] = useState<boolean>(false)
+  const [sdkInitialized, setSdkInitialized] = useState<boolean | null>(null)
+  const [isInitializing, setIsInitializing] = useState<boolean>(false)
 
   useEffect(() => {
     const initializeSDK = async () => {
+      setIsInitializing(true)
+
       try {
         await initializeFaceBookSDK()
         setSdkInitialized(true)
       } catch (error) {
         console.error('Failed to initialize Facebook SDK', error)
         setSdkInitialized(false)
+      } finally {
+        setIsInitializing(false)
       }
     }
     initializeSDK()
@@ -92,10 +104,11 @@ export default function LinkChannelDropdown() {
         <DropdownMenuItem
           disabled={!sdkInitialized}
           onSelect={handleWhatsAppSignup}
-          className='cursor-pointer'
+          className="cursor-pointer"
         >
-          <FaWhatsapp className='mr-2 h-4 w-4' />
+          <FaWhatsapp className="mr-2 h-4 w-4" />
           WhatsApp
+          {isInitializing ? <FiLoader className="ml-2 h-4 w-4" /> : sdkInitialized === false && <CiWarning className="ml-2 h-4 w-4" />}
         </DropdownMenuItem>
         <DropdownMenuItem
           disabled
