d4079ddee349b48ac6165efd1388f3a7dc1c3eb4
maina-david <waihura.m@gmail.com>
2023-10-27 02:23:36 +0300

    update contact schema

--- lib/validations/contact.ts
+++ lib/validations/contact.ts
@@ -1,8 +1,8 @@
 import * as z from "zod"
 
 export const createContactSchema = z.object({
-    name: z.string().min(2).max(50),
-    alias: z.string().min(2).max(50).optional(),
-    channel: z.enum(['WHATSAPP']),
-    identifier: z.string().min(1)
-  })
+  name: z.string().min(2).max(50),
+  alias: z.string().optional(),
+  channel: z.enum(['WHATSAPP']),
+  identifier: z.string().min(1)
+})
