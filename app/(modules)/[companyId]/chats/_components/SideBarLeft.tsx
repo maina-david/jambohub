2649421ca01f6caa39e318db40497d75768adbfe
maina-david <waihura.m@gmail.com>
2023-10-28 00:16:44 +0300

    update sidebar scroll area

--- app/(modules)/[companyId]/chats/_components/SideBarLeft.tsx
+++ app/(modules)/[companyId]/chats/_components/SideBarLeft.tsx
@@ -53,95 +53,97 @@ const SideBarLeft = (props: SideBarLeftProps) => {
           <Input placeholder="Search for contact..." />
         </div>
         <Separator />
-        <ScrollArea className="h-[470px] space-y-4">
-          <h5 className="mb-3.5 ml-3 text-xl font-semibold tracking-tight">
-            Chats
-          </h5>
-          <div className="mb-5 truncate">
+        <div className="flex h-[470px] flex-col overflow-hidden">
+          <ScrollArea className="flex-1 overflow-y-auto">
+            <h5 className="mb-3.5 ml-3 text-xl font-semibold tracking-tight">
+              Chats
+            </h5>
+            <div className="mb-5 truncate">
+              <AnimatePresence>
+                {chats.length > 0 ? (
+                  chats.map((chat) => (
+                    <motion.div
+                      key={chat.id}
+                      initial={{ opacity: 0, y: "100%" }}
+                      animate={{ opacity: 1, y: 0 }}
+                      className={cn(
+                        "flex cursor-pointer flex-row items-start rounded-2xl px-3 py-2 hover:bg-accent",
+                        chat.contactId === selectedChat?.contactId && "bg-accent"
+                      )}
+                      onClick={() => setSelectedChat(chat.contactId)}
+                    >
+                      <UserAvatar
+                        user={{ name: chat.Contact.name || null, image: null }}
+                        className="mr-2 h-8 w-8"
+                      />
+                      <div className="flex-1 truncate">
+                        <p className="truncate text-base font-medium tracking-tight">
+                          {chat.Contact.name || chat.Contact.identifier}
+                        </p>
+                        <p className="truncate">
+                          {getLastChatMessage(chat) &&
+                            getLastChatMessage(chat)!.message!.length > 20
+                            ? `${getLastChatMessage(chat)?.message.slice(0, 20)}...`
+                            : getLastChatMessage(chat)?.message
+                          }
+                        </p>
+                      </div>
+                      <div className='flex flex-col items-end'>
+                        <p className="whitespace-nowrap text-sm text-gray-500">
+                          {getLastChatMessage(chat) ? formatTimestamp(getLastChatMessage(chat)?.timestamp) : formatTimestamp()}
+                        </p>
+                        {chat.unreadMessageCount > 0 && (
+                          <span className="rounded-full bg-red-500 p-1 text-xs text-white">
+                            {chat.unreadMessageCount}
+                          </span>
+                        )}
+                      </div>
+                    </motion.div>
+                  ))
+                ) : (
+                  <p className="mt-6 text-center text-sm">
+                    No chats available
+                  </p>
+                )}
+              </AnimatePresence>
+            </div>
+            <div className='flex items-center gap-2'>
+              <h5 className="flex items-center text-xl font-semibold">
+                Contacts
+              </h5>
+              <div className="grow"></div>
+              <AddContactDialog />
+            </div>
             <AnimatePresence>
-              {chats.length > 0 ? (
-                chats.map((chat) => (
+              {contacts.length > 0 ? (
+                contacts.map((contact) => (
                   <motion.div
-                    key={chat.id}
+                    key={contact.id}
                     initial={{ opacity: 0, y: "100%" }}
                     animate={{ opacity: 1, y: 0 }}
-                    className={cn(
-                      "flex cursor-pointer flex-row items-start rounded-2xl px-3 py-2 hover:bg-accent",
-                      chat.contactId === selectedChat?.contactId && "bg-accent"
-                    )}
-                    onClick={() => setSelectedChat(chat.contactId)}
+                    className="w-full cursor-pointer px-3 py-2 hover:bg-accent"
+                    onClick={() => setSelectedChat(contact.id)}
                   >
-                    <UserAvatar
-                      user={{ name: chat.Contact.name || null, image: null }}
-                      className="mr-2 h-8 w-8"
-                    />
-                    <div className="flex-1 truncate">
-                      <p className="truncate text-base font-medium tracking-tight">
-                        {chat.Contact.name || chat.Contact.identifier}
-                      </p>
-                      <p className="truncate">
-                        {getLastChatMessage(chat) &&
-                          getLastChatMessage(chat)!.message!.length > 20
-                          ? `${getLastChatMessage(chat)?.message.slice(0, 20)}...`
-                          : getLastChatMessage(chat)?.message
-                        }
-                      </p>
-                    </div>
-                    <div className='flex flex-col items-end'>
-                      <p className="whitespace-nowrap text-sm text-gray-500">
-                        {getLastChatMessage(chat) ? formatTimestamp(getLastChatMessage(chat)?.timestamp) : formatTimestamp()}
-                      </p>
-                      {chat.unreadMessageCount > 0 && (
-                        <span className="rounded-full bg-red-500 p-1 text-xs text-white">
-                          {chat.unreadMessageCount}
-                        </span>
-                      )}
+                    <div className='flex flex-row items-start'>
+                      <UserAvatar
+                        user={{ name: contact.alias || null, image: null }}
+                        className="mr-2 h-8 w-8"
+                      />
+                      <div className='flex-1 flex-col'>
+                        <p className="text-base font-medium tracking-tight">{contact.name || contact.identifier}</p>
+                        <p className="text-sm tracking-tight">{contact.alias}</p>
+                      </div>
                     </div>
                   </motion.div>
                 ))
               ) : (
                 <p className="mt-6 text-center text-sm">
-                  No chats available
+                  No contacts available
                 </p>
               )}
             </AnimatePresence>
-          </div>
-          <div className='flex items-center gap-2'>
-            <h5 className="flex items-center text-xl font-semibold">
-              Contacts
-            </h5>
-            <div className="grow"></div>
-            <AddContactDialog />
-          </div>
-          <AnimatePresence>
-            {contacts.length > 0 ? (
-              contacts.map((contact) => (
-                <motion.div
-                  key={contact.id}
-                  initial={{ opacity: 0, y: "100%" }}
-                  animate={{ opacity: 1, y: 0 }}
-                  className="w-full cursor-pointer px-3 py-2 hover:bg-accent"
-                  onClick={() => setSelectedChat(contact.id)}
-                >
-                  <div className='flex flex-row items-start'>
-                    <UserAvatar
-                      user={{ name: contact.alias || null, image: null }}
-                      className="mr-2 h-8 w-8"
-                    />
-                    <div className='flex-1 flex-col'>
-                      <p className="text-base font-medium tracking-tight">{contact.name || contact.identifier}</p>
-                      <p className="text-sm tracking-tight">{contact.alias}</p>
-                    </div>
-                  </div>
-                </motion.div>
-              ))
-            ) : (
-              <p className="mt-6 text-center text-sm">
-                No contacts available
-              </p>
-            )}
-          </AnimatePresence>
-        </ScrollArea>
+          </ScrollArea>
+        </div>
       </>
     )
   }
