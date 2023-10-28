46e5f055f94f25f32bbc58426b672a484ad5c3e4
maina-david <waihura.m@gmail.com>
2023-10-28 21:55:41 +0300

    update flow sidebar hovercards

--- app/playground/[companyId]/flow/[flowId]/components/SideBar.tsx
+++ app/playground/[companyId]/flow/[flowId]/components/SideBar.tsx
@@ -45,7 +45,7 @@ export default function SideBar() {
                 className="w-[260px] text-sm"
                 side="left"
               >
-                {/* Detailed Node description here */}
+                This sends a text message to the user and proceeds to the next node in the flow. It does not wait for response from the user
               </HoverCardContent>
             </HoverCard>
             <p className="text-sm text-muted-foreground">
@@ -61,9 +61,18 @@ export default function SideBar() {
           <div
             className="grid w-full items-center gap-1.5"
           >
-            <p className="text-sm font-medium leading-none">
-              Send Text And Wait
-            </p>
+            <HoverCard openDelay={200}>
+              <HoverCardTrigger asChild>
+                <Label htmlFor="node">Send Text And Wait</Label>
+              </HoverCardTrigger>
+              <HoverCardContent
+                align="start"
+                className="w-[260px] text-sm"
+                side="left"
+              >
+                This sends a text message to the user and waits for a response from the user. The flow does not proceed without a response being received from user.
+              </HoverCardContent>
+            </HoverCard>
             <p className="text-sm text-muted-foreground">
               Drag this to send text and wait for a response.
             </p>
@@ -77,9 +86,18 @@ export default function SideBar() {
           <div
             className="grid w-full items-center gap-1.5"
           >
-            <p className="text-sm font-medium leading-none">
-              Send Text Response
-            </p>
+            <HoverCard openDelay={200}>
+              <HoverCardTrigger asChild>
+                <Label htmlFor="node">Send Text Response</Label>
+              </HoverCardTrigger>
+              <HoverCardContent
+                align="start"
+                className="w-[260px] text-sm"
+                side="left"
+              >
+                This processes the response sent by user and returns a text response to the user.
+              </HoverCardContent>
+            </HoverCard>
             <p className="text-sm text-muted-foreground">
               Drag this to send text response to the user.
             </p>
@@ -93,9 +111,18 @@ export default function SideBar() {
           <div
             className="grid w-full items-center gap-1.5"
           >
-            <p className="text-sm font-medium leading-none">
-              Send Response And Wait
-            </p>
+            <HoverCard openDelay={200}>
+              <HoverCardTrigger asChild>
+                <Label htmlFor="node">Send Response And Wait</Label>
+              </HoverCardTrigger>
+              <HoverCardContent
+                align="start"
+                className="w-[260px] text-sm"
+                side="left"
+              >
+                This handles a response from the user and responds to the user then wait for a response back from user. The flow will not proceed without a response from user.
+              </HoverCardContent>
+            </HoverCard>
             <p className="text-sm text-muted-foreground">
               Drag this to send response and wait for a response from user.
             </p>
@@ -109,9 +136,18 @@ export default function SideBar() {
           <div
             className="grid w-full items-center gap-1.5"
           >
-            <p className="text-sm font-medium leading-none">
-              Assign To Team
-            </p>
+            <HoverCard openDelay={200}>
+              <HoverCardTrigger asChild>
+                <Label htmlFor="node">Assign To Team</Label>
+              </HoverCardTrigger>
+              <HoverCardContent
+                align="start"
+                className="w-[260px] text-sm"
+                side="left"
+              >
+                This assigns user to a team for interactive conversation. It will use round-robin method to allocate conversation to team member
+              </HoverCardContent>
+            </HoverCard>
             <p className="text-sm text-muted-foreground">
               Drag this to assign the user to a team.
             </p>
@@ -125,9 +161,18 @@ export default function SideBar() {
           onDragStart={(event) => onDragStart(event, 'sendAttachment')}
         >
           <div className="grid w-full items-center gap-1.5">
-            <p className="text-sm font-medium leading-none">
-              Send Attachment
-            </p>
+            <HoverCard openDelay={200}>
+              <HoverCardTrigger asChild>
+                <Label htmlFor="node">Send Attachment</Label>
+              </HoverCardTrigger>
+              <HoverCardContent
+                align="start"
+                className="w-[260px] text-sm"
+                side="left"
+              >
+                This sends a pre-uploaded file attachment in the current company
+              </HoverCardContent>
+            </HoverCard>
             <p className="text-sm text-muted-foreground">
               Drag this to send a file attachment response.
             </p>
@@ -153,9 +198,18 @@ export default function SideBar() {
           onDragStart={(event) => onDragStart(event, 'performDisbursement')}
         >
           <div className="grid w-full items-center gap-1.5">
-            <p className="text-sm font-medium leading-none">
-              Perform Disbursement
-            </p>
+            <HoverCard openDelay={200}>
+              <HoverCardTrigger asChild>
+                <Label htmlFor="node">Perform Disbursment</Label>
+              </HoverCardTrigger>
+              <HoverCardContent
+                align="start"
+                className="w-[260px] text-sm"
+                side="left"
+              >
+                This disburses funds to the user
+              </HoverCardContent>
+            </HoverCard>
             <p className="text-sm text-muted-foreground">
               Drag this to perform a disbursement transaction.
             </p>
@@ -167,9 +221,18 @@ export default function SideBar() {
           onDragStart={(event) => onDragStart(event, 'authenticateUser')}
         >
           <div className="grid w-full items-center gap-1.5">
-            <p className="text-sm font-medium leading-none">
-              Authenticate User
-            </p>
+            <HoverCard openDelay={200}>
+              <HoverCardTrigger asChild>
+                <Label htmlFor="node">Authenticate User</Label>
+              </HoverCardTrigger>
+              <HoverCardContent
+                align="start"
+                className="w-[260px] text-sm"
+                side="left"
+              >
+                This authenticates user requesting password or pin then proceeds with flow
+              </HoverCardContent>
+            </HoverCard>
             <p className="text-sm text-muted-foreground">
               Drag this to authenticate the user.
             </p>
@@ -181,9 +244,18 @@ export default function SideBar() {
           onDragStart={(event) => onDragStart(event, 'customAction')}
         >
           <div className="grid w-full items-center gap-1.5">
-            <p className="text-sm font-medium leading-none">
-              Custom Action
-            </p>
+            <HoverCard openDelay={200}>
+              <HoverCardTrigger asChild>
+                <Label htmlFor="node">Custom Action</Label>
+              </HoverCardTrigger>
+              <HoverCardContent
+                align="start"
+                className="w-[260px] text-sm"
+                side="left"
+              >
+                {/* To update later */}
+              </HoverCardContent>
+            </HoverCard>
             <p className="text-sm text-muted-foreground">
               Drag this to perform a custom action.
             </p>
