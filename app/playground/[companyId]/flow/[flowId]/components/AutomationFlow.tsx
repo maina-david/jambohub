dc1f5bba526708849be8dbb0c8f679fbf572f42a
maina-david <waihura.m@gmail.com>
2023-10-29 04:00:14 +0300

    update flow validation

--- app/playground/[companyId]/flow/[flowId]/components/AutomationFlow.tsx
+++ app/playground/[companyId]/flow/[flowId]/components/AutomationFlow.tsx
@@ -124,6 +124,7 @@ function FlowArea({ flowData, flowErrors }) {
       reactFlowInstance.setViewport({ x, y, zoom })
       if(flowErrors){
         for(const flowError of flowErrors){
+          console.log("flowError: ", flowError)
           updateNodeColor(flowError.id, '#FF0000')
         }
       }
