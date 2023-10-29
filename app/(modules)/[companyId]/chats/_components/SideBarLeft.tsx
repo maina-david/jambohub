2e9947474c1d023d1ba51cb1ccfd5942d5baf30a
maina-david <waihura.m@gmail.com>
2023-10-29 13:46:57 +0300

    add sidebar fixed height

--- app/(modules)/[companyId]/chats/_components/SideBarLeft.tsx
+++ app/(modules)/[companyId]/chats/_components/SideBarLeft.tsx
@@ -200,7 +200,7 @@ const SideBarLeft = (props: SideBarLeftProps) => {
 
   if (isMdAndAbove) {
     return (
-      <div className="h-[85vh] min-h-[85vh] w-1/3 flex-1 flex-col border-r">
+      <div className="h-[80vh] min-h-[80vh] w-1/3 flex-1 flex-col border-r">
         {renderChatsAndContacts()}
       </div>
     )
