e9794011eb460061b020412e0296aaea302fe48e
maina-david <waihura.m@gmail.com>
2023-10-29 12:20:54 +0300

    add sidebar fixed height

--- app/(modules)/[companyId]/chats/_components/SideBarLeft.tsx
+++ app/(modules)/[companyId]/chats/_components/SideBarLeft.tsx
@@ -154,7 +154,7 @@ const SideBarLeft = (props: SideBarLeftProps) => {
             <div className="grow"></div>
             <AddContactDialog />
           </div>
-          <ScrollArea className="h-[30vh] min-h-[30vh] flex-1">
+          <ScrollArea className="h-[25vh] min-h-[25vh] flex-1">
             <AnimatePresence>
               {contacts.length > 0 ? (
                 contacts.map((contact) => (
