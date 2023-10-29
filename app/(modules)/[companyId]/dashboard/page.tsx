52497c05664702e78ea7779100c2c3b098f13275
maina-david <waihura.m@gmail.com>
2023-10-29 16:33:18 +0300

    add header on dashboard

--- app/(modules)/[companyId]/dashboard/page.tsx
+++ app/(modules)/[companyId]/dashboard/page.tsx
@@ -1,5 +1,7 @@
+import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import React from 'react'
+import { CalendarDateRangePicker } from './_components/date-range-picker'
 
 export const metadata = {
   title: "Dashboard",
@@ -7,6 +9,14 @@ export const metadata = {
 
 function DashboardPage() {
   return (
+    <div className="flex-1 space-y-4 p-8 pt-6">
+      <div className="flex items-center justify-between space-y-2">
+        <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
+        <div className="flex items-center space-x-2">
+          <CalendarDateRangePicker />
+          <Button>Download</Button>
+        </div>
+      </div>
     <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
       <Card>
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
@@ -109,6 +119,7 @@ function DashboardPage() {
           </p>
         </CardContent>
       </Card>
+    </div>
     </div>
   )
 }
