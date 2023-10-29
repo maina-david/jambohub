1f3cbfc6d335a97264a1df7092710783cf8cfef7
maina-david <waihura.m@gmail.com>
2023-10-29 15:21:29 +0300

    update chatarea height

--- app/(modules)/[companyId]/chats/_components/ChatContentArea.tsx
+++ app/(modules)/[companyId]/chats/_components/ChatContentArea.tsx
@@ -174,7 +174,7 @@ const ChatContentArea: React.FC<ChatContentAreaProps> = ({
   }
 
   return (
-    <div className={cn('flex h-[80vh] min-h-[80vh] flex-col', isMdAndAbove ? 'w-2/3' : 'w-full')}>
+    <div className={cn('flex h-[85vh] min-h-[85vh] flex-col', isMdAndAbove ? 'w-2/3' : 'w-full')}>
       {selectedChat ? (
         <>
           <div className="flex items-center justify-between px-5 py-2.5">
