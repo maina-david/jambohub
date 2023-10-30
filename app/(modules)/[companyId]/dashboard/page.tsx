8dfa5bdb3f4cbe1db8306ec6ec988ebc311da8a3
maina-david <waihura.m@gmail.com>
2023-10-30 15:55:47 +0300

    update dashboard page

--- app/(modules)/[companyId]/dashboard/page.tsx
+++ app/(modules)/[companyId]/dashboard/page.tsx
@@ -2,6 +2,7 @@ import { Button } from '@/components/ui/button'
 import React from 'react'
 import { CalendarDateRangePicker } from './_components/date-range-picker'
 import DashboardStatsCards from './_components/stats-cards'
+import { FilterIcon } from 'lucide-react'
 
 export const metadata = {
   title: "Dashboard",
@@ -15,7 +16,10 @@ export default function DashboardPage() {
           <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
           <div className="flex items-center space-x-2">
             <CalendarDateRangePicker />
-            <Button>Download</Button>
+            <Button>
+              <FilterIcon className='mr-2 h-4 w-4' />
+              Filter
+            </Button>
           </div>
         </div>
         <DashboardStatsCards />
