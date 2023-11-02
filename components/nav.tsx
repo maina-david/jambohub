f05f2002e364231ed7121bc51047918c2aae118a
maina-david <waihura.m@gmail.com>
2023-11-02 14:39:33 +0300

    update nav scroll area

--- components/nav.tsx
+++ components/nav.tsx
@@ -145,7 +145,7 @@ export function SideNav(props: SideNavProps) {
   }
   const renderSideNav = () => {
     return (
-      <ScrollArea className="h-72 w-full">
+      <ScrollArea className="h-[80vh] w-full">
         <AnimatePresence>
           <motion.nav
             initial="closed"
